05d555bbf08ecac4f3f73c8dec20d8eccef5227a
Jorg Van Renterghem <jorg.vanrenterghem@ugent.be>
2017-03-06 22:30:47 +0100

    Add small difference cause commits are usefulltreetree

--- app/backend/src/main/java/controler/VehicleControler.java
+++ app/backend/src/main/java/controler/VehicleControler.java
@@ -8,6 +8,7 @@ import model.fleet.Vehicle;
 
 import java.time.LocalDate;
 import java.util.Collection;
+import java.util.UUID;
 
 /**
  * Created by jorg on 3/6/17.
@@ -20,27 +21,29 @@ public class VehicleControler {
         vehicleDAO=new TestVehicleDAO();
     }
 
-    public Vehicle get(int id) throws DataAccessException {
-        return vehicleDAO.get(id);
+    public Vehicle get(String id) throws DataAccessException {
+        return vehicleDAO.get(UUID.fromString(id));
     }
 
 
     public void update(Vehicle vehicle) throws DataAccessException {
-
+        vehicleDAO.update(vehicle);
     }
 
 
     public void remove(Vehicle vehicle) throws DataAccessException {
-
+        vehicleDAO.remove(vehicle);
     }
 
 
     public Collection<Vehicle> listFiltered(Filter... filters) throws DataAccessException {
-        return null;
+        return vehicleDAO.listFiltered(filters);
     }
 
 
-    public Vehicle create(String brand, String model, String licensePlate, LocalDate productionDate, String chassisNumber, int value, int mileage) {
-        return null;
+    public Vehicle create(String brand, String model, String licensePlate, LocalDate productionDate, String chassisNumber, int mileage) throws DataAccessException {
+        Vehicle vehicle=new Vehicle(UUID.randomUUID(),brand,model,licensePlate,productionDate,chassisNumber,0,mileage); //TODO value
+        vehicleDAO.update(vehicle);
+        return vehicle;
     }
 }
