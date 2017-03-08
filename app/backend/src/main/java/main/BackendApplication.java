a92ead74980f28add0f5d2cf97ce34edd25bcb73
Jorg Van Renterghem <jorg.vanrenterghem@ugent.be>
2017-03-08 17:55:56 +0100

    Fix build bug

--- app/backend/src/main/java/main/BackendApplication.java
+++ app/backend/src/main/java/main/BackendApplication.java
@@ -3,8 +3,6 @@ package main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 
 @SpringBootApplication
