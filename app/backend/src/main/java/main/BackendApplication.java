acba0bf3f03ced550f61749c6f3e6cbdc717bbb0
Jorg Van Renterghem <jorg.vanrenterghem@ugent.be>
2017-03-08 17:48:29 +0100

    Fix app security issue

--- app/backend/src/main/java/main/BackendApplication.java
+++ app/backend/src/main/java/main/BackendApplication.java
@@ -3,6 +3,9 @@ package main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"spring"})
