345aff70a81c4a24e82e0ba7f78d0886aca94021
Jarre Knockaert <jarknock@ugent.be>
2017-03-09 13:10:45 +0100

    add backend support

--- app/frontend/website/src/main.js
+++ app/frontend/website/src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import VueI18n from 'vue-i18n'
 import routes from './config/routes'
 
+//Routing support
 Vue.use(VueRouter)
+//Backend support
+Vue.use(VueResource)
+//Language support
 Vue.use(VueI18n)
 
 Vue.config.lang = 'nl'
