5a21d8c66c6239f4bf6770b84dc08a6e9f755f89
Jorg Van Renterghem <jorg.vanrenterghem@ugent.be>
2017-03-10 15:37:54 +0100

    Fix exceptionhandler

--- app/backend/src/main/java/spring/Exceptions/MyExceptionHandler.java
+++ app/backend/src/main/java/spring/Exceptions/MyExceptionHandler.java
@@ -1,21 +1,20 @@
 package spring.Exceptions;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import spring.model.RESTError;
+
 
 /**
  * Created by jorg on 3/10/17.
  */
 @ControllerAdvice
-public class MyExceptionHandler extends ResponseEntityExceptionHandler {
+public class MyExceptionHandler {
+
 
     @ExceptionHandler(AttributeException.class)
     protected ResponseEntity<Object> handleAttributeException(AttributeException attributeException) {
@@ -24,9 +23,7 @@ public class MyExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    protected ResponseEntity handleBadInput(HttpMessageNotReadableException ex) {
-        return new ResponseEntity(HttpStatus.OK);
-        //throw new JSONMalformedException();
+    protected ResponseEntity<Object> handleBadInput(HttpMessageNotReadableException ex) {
+        return ResponseEntity.badRequest().body(new RESTError(400,"The JSON is malformed. This is a syntax error.",""));
     }
-
 }
