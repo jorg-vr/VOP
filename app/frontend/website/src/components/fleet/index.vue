ab2d80161d3e1594e66b8c6784e1194696a5ae64
Jarre Knockaert <jarre.knockaert@ugent.be>
2017-03-12 15:34:40 +0100

    add searchbar

--- app/frontend/website/src/components/fleet/index.vue
+++ app/frontend/website/src/components/fleet/index.vue
@@ -4,35 +4,22 @@
             <h1> Vloten  </h1>
         </div>
         <div>
-            <div class="row">
-                <div class="col-md-6">
-                    <div id="search_input">
-                        <div class="input-group col-md-12">
-                            <input type="text" class="form-control input-mg" placeholder="Vloot zoeken" />
-                            <span class="input-group-btn">
-                                <button class="btn btn-info btn-mg" type="button">
-                                    <i class="fa fa-search" aria-hidden="true"></i>
-                                </button>
-	                        </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <router-link :to="{name: 'new_fleet'}"><button type="button" class="btn btn-primary table-button">Niew vloot</button></router-link>
+            <fleet-searchbar :fleets="fleets" v-on:fleetsChanged="updateFleetTable"></fleet-searchbar>
             <div class="row">
                 <div class="col-md-8">
                     <table class="table">
-                        <fleet-row v-for="fleet in fleets" :fleet="fleet" :key="fleet.id"></fleet-row>
+                        <fleet-row v-for="fleet in filteredFleets" :fleet="fleet" :key="fleet.id"></fleet-row>
                     </table>
                 </div>
             </div>
-
         </div>
     </div>
 </template>
 <script>
+    import FleetSearchBar from './FleetSearch.vue'
     export default {
         components: {
+            'fleet-searchbar' : FleetSearchBar,
             FleetRow: {
                 props: {
                     fleet: Object
@@ -63,17 +50,22 @@
                     {id: 2, company : 'Test company 2'},
                     {id: 3, company : 'Test company 3'},
                     {id: 4, company : 'Test company 4'}
-                ]
+                ],
+                filteredFleets : []
             }
         },
         created() {
             this.fetchFleetList()
+            this.filteredFleets =  this.fleets
         },
         methods: {
             fetchFleetList (){
                 this.$http.get('https://vopro5.ugent.be/app/api/fleets').then(response => {
-                    this.fleets = response.body
+                    this.fleets = response.body;
                 })
+            },
+            updateFleetTable(fleets){
+                this.filteredFleets = fleets;
             }
         }
     }
