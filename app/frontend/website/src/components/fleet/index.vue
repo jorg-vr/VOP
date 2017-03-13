48b859d29e17c22066cb1e936ea49405c247ab69
Tomas Van Roose <tomas.vanroose@ugent.be>
2017-03-13 16:08:15 +0100

    Add list row component
    Copy fleet index to fleet/index_cp.vue
    Add list row component to new fleet index

--- app/frontend/website/src/components/fleet/index.vue
+++ app/frontend/website/src/components/fleet/index.vue
@@ -3,76 +3,47 @@
         <div class="page-header">
             <h1>Vloten </h1>
         </div>
-        <div>
-            <fleet-searchbar :fleets="fleets" v-on:fleetsChanged="updateFleetTable"></fleet-searchbar>
-            <div class="row">
-                <div class="col-md-8">
-                    <table class="table">
-                        <fleet-row v-for="fleet in filteredFleets" :fleet="fleet" :key="fleet.id"></fleet-row>
-                    </table>
-                </div>
-            </div>
+        <div class="row">
+            <info-pane></info-pane>
+            <info-pane></info-pane>
         </div>
+        <button type="button" class="btn btn-circle btn-lg"><span class="glyphicon glyphicon-plus"></span></button>
     </div>
 </template>
+
 <script>
-    import FleetSearchBar from './FleetSearch.vue'
-    export default {
-        components: {
-            'fleet-searchbar' : FleetSearchBar,
-            FleetRow: {
-                props: {
-                    fleet: Object
-                },
-                template: `
-                <tr>
-                <td class="id-column">{{fleet.id}}</td>
-                <td class="full-width">{{fleet.company}}</td>
-                <td><router-link :to="{name: 'fleet', params: { id: fleet.id }}">
-                    <button class="btn btn-xs btn-warning"><i class="fa fa-eye" aria-hidden="true"></i></button>
-                </router-link></td>
-                <td><button v-on:click="removeFleet(fleet.id)" class="btn btn-xs btn-danger"><i class="fa fa-trash" aria-hidden="true"></i></button></td>
-                </tr>
-                `,
-                methods: {
-                    removeFleet (fleetID){
-                        this.$http.delete('https://vopro5.ugent.be/app/api/fleets/' + fleetID).then(response => {
-                            //Verwerk response
-                        })
-                    }
-                }
-            }
-        },
-        data() {
-            return {
-                fleets : [ //Some test fleets, this will be filled in with the actual fleets
-                    {id: 1, company : 'Test company 1'},
-                    {id: 2, company : 'Test company 2'},
-                    {id: 3, company : 'Test company 3'},
-                    {id: 4, company : 'Test company 4'}
-                ],
-                filteredFleets : []
-            }
-        },
-        created() {
-            this.fetchFleetList()
-            this.filteredFleets =  this.fleets
-        },
-        methods: {
-            fetchFleetList (){
-                this.$http.get('https://vopro5.ugent.be/app/api/fleets').then(response => {
-                    this.fleets = response.body;
-                })
-            },
-            updateFleetTable(fleets){
-                this.filteredFleets = fleets;
-            }
+export default {
+    components: {
+        infoPane: {
+            template: `
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        {{ fleets }}
+                    </div>
+                </div>
+                `
+        }
+    },
+    data: function() {
+        return {
+            fleets : [ //dummy fleets
+            {id: 1, company : 'Test company 1'},
+            {id: 2, company : 'Test company 2'},
+            {id: 3, company : 'Test company 3'},
+            {id: 4, company : 'Test company 4'}
+            ]
         }
     }
-</script>
+}
 
+</script>
 <style>
-    .id-column {
-        border-right: 1px solid #ddd;
-    }
+.btn-circle.btn-lg {
+  width: 50px;
+  height: 50px;
+  padding: 10px 16px;
+  font-size: 18px;
+  line-height: 1.33;
+  border-radius: 25px;
+}
 </style>
---
+++ app/frontend/website/src/components/listComponent.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            Name here
+        </div>
+    </div>
+</template>
