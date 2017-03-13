7bc2cb7314803dad0f0d8c1481190678aa279c1a
Sam Persoon <sam.persoon@ugent.be>
2017-03-13 13:24:49 +0100

    Add ProductionFleetDAO

--- app/backend/src/main/java/dao/interfaces/FleetDAO.java
+++ app/backend/src/main/java/dao/interfaces/FleetDAO.java
@@ -1,8 +1,17 @@
 package dao.interfaces;
 
+import model.fleet.Vehicle;
 import model.identity.Customer;
 import model.fleet.Fleet;
 
+import java.util.Collection;
+import java.util.UUID;
+
 public interface FleetDAO extends DAO<Fleet> {
+
+    public Fleet create(Customer customer, Collection<Vehicle> vehicles) throws DataAccessException;
+
+    public Fleet update(UUID id, Customer customer, Collection<Vehicle> vehicles) throws DataAccessException;
+
     Filter<Fleet> byOwner(Customer customer);
 }
