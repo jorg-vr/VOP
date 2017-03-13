63301393113844a6efd1a097d57f9553c9a53a7d
tdpontie <thomas.depontieu@ugent.be>
2017-03-13 22:36:22 +0100

    Add filter tests for ProductionVehicleDAO

---
+++ app/backend/src/test/java/dao/database/ProductionVehicleDAOFiltersTestTest.java
@@ -0,0 +1,179 @@
+package dao.database;
+
+import dao.interfaces.Filter;
+import model.fleet.Vehicle;
+import model.fleet.VehicleType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.Collection;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Ponti on 13/03/2017.
+ */
+public class ProductionVehicleDAOFiltersTestTest {
+    private ProductionProvider daoProvider;
+    private ProductionVehicleDAO vehicleDAO;
+    private Vehicle v1, v2, v3;
+    private VehicleType t1, t2;
+    private ProductionVehicleTypeDAO vehicleTypeDAO;
+
+
+    @Before
+    public void setUp() throws Exception {
+        daoProvider = (ProductionProvider) ProductionProvider.getInstance();
+        vehicleDAO = (ProductionVehicleDAO) daoProvider.getVehicleDAO();
+        vehicleTypeDAO = (ProductionVehicleTypeDAO) daoProvider.getVehicleTypeDAO();
+
+        t1 = vehicleTypeDAO.create("type 1", 2.5);
+        t2 = vehicleTypeDAO.create("type 2", 5.7);
+        v1 = vehicleDAO.create("brand 1", "model 1", "AAAAAAAAAAAAAAAAA", "ABC-123", 500, 3000, t1, LocalDate.of(2016, 7, 15));
+        v2 = vehicleDAO.create("brand 1", "model 2", "BBBBBBBBBBBBBBBBB", "DEF-123", 1000, 3500, t2, LocalDate.of(2016, 7, 26));
+        v3 = vehicleDAO.create("brand 2", "model 2", "CCCCCCCCCCCCCCCCC", "DEF-456", 1500, 4000, t1, LocalDate.of(2016, 9, 26));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        vehicleDAO.remove(v1.getUuid());
+        vehicleDAO.remove(v2.getUuid());
+        vehicleDAO.remove(v3.getUuid());
+        vehicleTypeDAO.remove(t1.getUuid());
+        vehicleTypeDAO.remove(t2.getUuid());
+    }
+
+    @Ignore
+    @Test
+    public void byBrand() throws Exception {
+        Filter<Vehicle>[] f1 = new Filter[]{vehicleDAO.byBrand("brand 1")};
+        Filter<Vehicle>[] f2 = new Filter[]{vehicleDAO.byBrand("brand 2")};
+        Collection<Vehicle> c1 = vehicleDAO.listFiltered(f1);
+        Collection<Vehicle> c2 = vehicleDAO.listFiltered(f2);
+
+        assertTrue("byBrand filter werkt niet", c1.contains(v1) & c1.contains(v2) & (c1.size() == 2));
+        assertTrue("byBrand filter werkt niet", c2.contains(v3) & (c2.size() == 1));
+    }
+
+    @Ignore
+    @Test
+    public void byModel() throws Exception {
+        Filter<Vehicle>[] f1 = new Filter[]{vehicleDAO.byModel("model 1")};
+        Filter<Vehicle>[] f2 = new Filter[]{vehicleDAO.byModel("model 2")};
+        Collection<Vehicle> c1 = vehicleDAO.listFiltered(f1);
+        Collection<Vehicle> c2 = vehicleDAO.listFiltered(f2);
+
+        assertTrue("byModel filter werkt niet", c1.contains(v1) & (c1.size() == 1));
+        assertTrue("byModel filter werkt niet", c2.contains(v2) & c2.contains(v3) & (c2.size() == 2));
+    }
+
+    @Ignore
+    @Test
+    public void byLicensePlate() throws Exception {
+        Filter<Vehicle>[] f1 = new Filter[]{vehicleDAO.byLicensePlate("ABC-123")};
+        Filter<Vehicle>[] f2 = new Filter[]{vehicleDAO.byLicensePlate("ABC-456")};
+        Collection<Vehicle> c1 = vehicleDAO.listFiltered(f1);
+        Collection<Vehicle> c2 = vehicleDAO.listFiltered(f2);
+
+        assertTrue("byLicensePlate filter werkt niet", c1.contains(v1) & (c1.size() == 1));
+        assertTrue("byLicensePlate filter werkt niet", c2.contains(v3) & (c2.size() == 1));
+    }
+
+    @Ignore
+    @Test
+    public void atProductionDate() throws Exception {
+        Filter<Vehicle>[] f1 = new Filter[]{vehicleDAO.atProductionDate(LocalDate.of(2016, 7, 15))};
+        Filter<Vehicle>[] f2 = new Filter[]{vehicleDAO.atProductionDate(LocalDate.of(2016, 9, 26))};
+        Collection<Vehicle> c1 = vehicleDAO.listFiltered(f1);
+        Collection<Vehicle> c2 = vehicleDAO.listFiltered(f2);
+
+        assertTrue("atProductionDate filter werkt niet", c1.contains(v1) & (c1.size() == 1));
+        assertTrue("atProductionDate filter werkt niet", c2.contains(v3) & (c2.size() == 1));
+    }
+
+    @Ignore
+    @Test
+    public void beforeProductionDate() throws Exception {
+        Filter<Vehicle>[] f1 = new Filter[]{vehicleDAO.beforeProductionDate(LocalDate.of(2016, 7, 27))};
+        Filter<Vehicle>[] f2 = new Filter[]{vehicleDAO.beforeProductionDate(LocalDate.of(2017, 7, 15))};
+        Collection<Vehicle> c1 = vehicleDAO.listFiltered(f1);
+        Collection<Vehicle> c2 = vehicleDAO.listFiltered(f2);
+
+        assertTrue("beforeProductionDate filter werkt niet", c1.contains(v1) & c1.contains(v2) & (c1.size() == 2));
+        assertTrue("beforeProductionDate filter werkt niet", c2.size() == 3);
+    }
+
+    @Ignore
+    @Test
+    public void afterProductionDate() throws Exception {
+        Filter<Vehicle>[] f1 = new Filter[]{vehicleDAO.afterProductionDate(LocalDate.of(2016, 7, 16))};
+        Filter<Vehicle>[] f2 = new Filter[]{vehicleDAO.afterProductionDate(LocalDate.of(2017, 7, 15))};
+        Collection<Vehicle> c1 = vehicleDAO.listFiltered(f1);
+        Collection<Vehicle> c2 = vehicleDAO.listFiltered(f2);
+
+        assertTrue("afterProductionDate filter werkt niet", c1.contains(v2) & c1.contains(v3) & (c1.size() == 2));
+        assertTrue("afterProductionDate filter werkt niet", c2.size() == 0);
+    }
+
+    @Ignore
+    @Test
+    public void atLeastMileage() throws Exception {
+        Filter<Vehicle>[] f1 = new Filter[]{vehicleDAO.atLeastMileage(3500)};
+        Filter<Vehicle>[] f2 = new Filter[]{vehicleDAO.atLeastMileage(4500)};
+        Collection<Vehicle> c1 = vehicleDAO.listFiltered(f1);
+        Collection<Vehicle> c2 = vehicleDAO.listFiltered(f2);
+
+        assertTrue("atLeastMileage filter werkt niet", c1.contains(v2) & c1.contains(v3) & (c1.size() == 2));
+        assertTrue("atLeastMileage filter werkt niet", c2.size() == 0);
+    }
+
+    @Ignore
+    @Test
+    public void maxMileage() throws Exception {
+        Filter<Vehicle>[] f1 = new Filter[]{vehicleDAO.maxMileage(3000)};
+        Filter<Vehicle>[] f2 = new Filter[]{vehicleDAO.maxMileage(4500)};
+        Collection<Vehicle> c1 = vehicleDAO.listFiltered(f1);
+        Collection<Vehicle> c2 = vehicleDAO.listFiltered(f2);
+
+        assertTrue("maxMileage filter werkt niet", c1.contains(v1) & (c1.size() == 1));
+        assertTrue("maxMileage filter werkt niet", c2.size() == 3);
+    }
+
+    @Ignore
+    @Test
+    public void byType() throws Exception {
+        Filter<Vehicle>[] f1 = new Filter[]{vehicleDAO.byType(t1)};
+        Filter<Vehicle>[] f2 = new Filter[]{vehicleDAO.byType(t2)};
+        Collection<Vehicle> c1 = vehicleDAO.listFiltered(f1);
+        Collection<Vehicle> c2 = vehicleDAO.listFiltered(f2);
+
+        assertTrue("byType filter werkt niet", c1.contains(v1) & c1.contains(v3) & (c1.size() == 2));
+        assertTrue("byType filter werkt niet", c2.contains(v2) & (c2.size() == 1));
+    }
+
+    @Ignore
+    @Test
+    public void multipleFilters() throws Exception {
+        Vehicle v4 = vehicleDAO.create("brand 1", "model 2", "FAAAAAAAAAAAAAAAA", "ABC-123", 500, 4000, t2, LocalDate.of(2016, 9, 28));
+        Vehicle v5 = vehicleDAO.create("brand 2", "model 1", "FBBBBBBBBBBBBBBBB", "DEF-123", 1000, 4500, t1, LocalDate.of(2017, 7, 26));
+        Vehicle v6 = vehicleDAO.create("brand 1", "model 2", "FCCCCCCCCCCCCCCCC", "DEF-456", 1500, 5000, t2, LocalDate.of(2017, 9, 26));
+        Filter<Vehicle>[] f1 = new Filter[]{vehicleDAO.byType(t1), vehicleDAO.byModel("model 1")};
+        Filter<Vehicle>[] f2 = new Filter[]{vehicleDAO.byBrand("brand 1"), vehicleDAO.byLicensePlate("DEF-123")};
+        Filter<Vehicle>[] f3 = new Filter[]{vehicleDAO.atLeastMileage(4000), vehicleDAO.beforeProductionDate(LocalDate.of(2016, 9, 28))};
+        Collection<Vehicle> c1 = vehicleDAO.listFiltered(f1);
+        Collection<Vehicle> c2 = vehicleDAO.listFiltered(f2);
+        Collection<Vehicle> c3 = vehicleDAO.listFiltered(f3);
+
+        assertTrue("Applying multiple filters simultaneously doesn't work correctly", c1.contains(v1) & c1.contains(v5) & (c1.size() == 2));
+        assertTrue("Applying multiple filters simultaneously doesn't work correctly", c2.contains(v2) & (c2.size() == 1));
+        assertTrue("Applying multiple filters simultaneously doesn't work correctly", c3.contains(v3) & (c3.size() == 1));
+
+        vehicleDAO.remove(v4.getUuid());
+        vehicleDAO.remove(v5.getUuid());
+        vehicleDAO.remove(v6.getUuid());
+    }
+
+}
--- app/backend/src/test/java/dao/database/ProductionVehicleDAOTest.java
+++ app/backend/src/test/java/dao/database/ProductionVehicleDAOTest.java
@@ -20,30 +20,34 @@ public class ProductionVehicleDAOTest {
 
     private ProductionProvider daoProvider;
     private ProductionVehicleDAO vehicleDao;
-    private Vehicle testvehicle;
+    private ProductionVehicleTypeDAO vehicleTypeDAO;
+    private VehicleType t1;
 
     //TODO: add the necessary setup code to be able to work with the database
     @Before
     public void setUp() throws Exception {
         daoProvider = (ProductionProvider) ProductionProvider.getInstance();
         vehicleDao = (ProductionVehicleDAO) daoProvider.getVehicleDAO();
+        vehicleTypeDAO = (ProductionVehicleTypeDAO) daoProvider.getVehicleTypeDAO();
+        t1 = vehicleTypeDAO.create("type 1", 2.5);
     }
 
     @After
     public void tearDown() throws Exception {
-
+        vehicleTypeDAO.remove(t1.getUuid());
     }
 
     @Ignore
     @Test
     public void createGetRemoveTest() throws Exception {
-        VehicleType type = new VehicleType(null, "type 1", 2.5);
+        //TODO: change it so new vehicletypes are created with the vehicletype DAO instead of through the vehicletype constructor
+        //VehicleType type = new VehicleType(null, "type 1", 2.5);
         Vehicle vehicle1 = null;
         boolean present = false;
         boolean removed = false;
         //test if a vehicle can be succesfully added to the database
         try {
-            vehicle1 = vehicleDao.create("brand 1", "model A", "UZ0UZABCUKZ12345L", "ABC 123", 30000, 2500, type, LocalDate.now());
+            vehicle1 = vehicleDao.create("brand 1", "model A", "UZ0UZABCUKZ12345L", "ABC 123", 30000, 2500, t1, LocalDate.now());
         } catch (Exception e) {
             fail("Failed trying to create a new vehicle");
         }
@@ -91,81 +95,14 @@ public class ProductionVehicleDAOTest {
     @Test
     public void update() throws Exception {
         //add new vehicle to the database
-        VehicleType type = new VehicleType(null, "type 2", 3.5);
-        Vehicle v1 = vehicleDao.create("brand 2", "model A", "AZ0UZABCUKZ12345L", "ABR 569", 36000, 4900, type, LocalDate.now());
+        Vehicle v1 = vehicleDao.create("brand 2", "model A", "AZ0UZABCUKZ12345L", "ABR 569", 36000, 4900, t1, LocalDate.now());
         //try to update the vehicle's brand field in the database
-        Vehicle v2 = vehicleDao.update(v1.getUuid(), "brand 3", "model A", "AZ0UZABCUKZ12345L", "ABR 569", 36000, 4900, type, v1.getProductionDate());
+        Vehicle v2 = vehicleDao.update(v1.getUuid(), "brand 3", "model A", "AZ0UZABCUKZ12345L", "ABR 569", 36000, 4900, t1, v1.getProductionDate());
         assertEquals("returned vehicle object is not updated", "brand 3", v2.getBrand());
         Vehicle v3 = vehicleDao.get(v1.getUuid());
         assertEquals("vehicle in the database is not updated", "brand 3", v3.getBrand());
         //clean up database for new other tests
         vehicleDao.remove(v1.getUuid());
-    }
-
-    @Ignore
-    @Test
-    public void listFiltered() throws Exception {
-
-    }
-
-    @Ignore
-    @Test
-    public void close() throws Exception {
-
-    }
-
-    @Ignore
-    @Test
-    public void byBrand() throws Exception {
-
-    }
-
-    @Ignore
-    @Test
-    public void byModel() throws Exception {
-
-    }
-
-    @Ignore
-    @Test
-    public void byLicensePlate() throws Exception {
-
-    }
-
-    @Ignore
-    @Test
-    public void atProductionDate() throws Exception {
-
-    }
-
-    @Ignore
-    @Test
-    public void beforeProductionDate() throws Exception {
-
-    }
-
-    @Ignore
-    @Test
-    public void afterProductionDate() throws Exception {
-
-    }
-
-    @Ignore
-    @Test
-    public void atLeastMileage() throws Exception {
-
-    }
-
-    @Ignore
-    @Test
-    public void maxMileage() throws Exception {
-
-    }
-
-    @Ignore
-    @Test
-    public void byType() throws Exception {
-
     }
 
 }
