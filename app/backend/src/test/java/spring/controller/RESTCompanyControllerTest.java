034656c09481bb2d79a916fd5c6989ff80888a2c
Jorg Van Renterghem <jorg.vanrenterghem@ugent.be>
2017-03-14 14:10:07 +0100

    Create test

---
+++ app/backend/src/test/java/spring/controller/RESTCompanyControllerTest.java
@@ -0,0 +1,36 @@
+package spring.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jorg on 3/14/17.
+ */
+public class RESTCompanyControllerTest {
+    @Test
+    public void get() throws Exception {
+
+    }
+
+    @Test
+    public void post() throws Exception {
+
+    }
+
+    @Test
+    public void getId() throws Exception {
+
+    }
+
+    @Test
+    public void putId() throws Exception {
+
+    }
+
+    @Test
+    public void deleteId() throws Exception {
+
+    }
+
+}
