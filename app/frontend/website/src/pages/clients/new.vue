de301afe84a0ac7464a17a10d94fc5669a032168
fversche <Freek.Verschelden@UGent.be>
2017-03-14 01:42:55 +0100

    Add new for clients

---
+++ app/frontend/website/src/pages/clients/new.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="content-wrapper">
+       <div class="page-header">
+         <h1> Klant aanmaken </h1>
+     </div>  
+     <form-temp :at=this.data :type=this.type></form-temp>
+
+ </div>
+
+</template>
+<script>
+    import FormTemp from './clientform.vue'
+    export default {
+        data(){
+            return {
+                data: ["Naam","Land","Plaats","Postcode","Straat","Nummer","BTW nummer","Telefoonnummer"],
+                type: "Klant"
+            }
+        },
+        components: { FormTemp},
+        created: function (){
+            // Keep reference to this Vue component 
+            var vm = this
+            // Listen to proceed performed by child component (Form.Vue)
+            this.$bus.$on('proceedAddClient', function(input){
+                console.log(input)
+                console.log('proceedAddClient called')
+
+                console.log('https://vopro5.ugent.be/app/api/companies/'+input)
+                this.$http.post('https://vopro5.ugent.be/app/api/companies', input, {
+
+                })
+                     // Make ajax request to server to create a User object
+                    //this.$http.post('https://vopro5.ugent.be/app/api/users').then(response => {
+                    //        this.clients = response.body
+                    //})
+                    vm.$router.go(-1)
+
+                });}
+        }  
+    </script>
+
