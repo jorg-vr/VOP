1ebb36aac1085cfb0f0644f0df35ff70aa3e11b8
fversche <Freek.Verschelden@UGent.be>
2017-03-14 14:59:59 +0100

    Adjust index for users

--- app/frontend/website/src/pages/users/index.vue
+++ app/frontend/website/src/pages/users/index.vue
@@ -3,62 +3,18 @@
     <div class="page-header">
     <h1> Gebruiker </h1>
     </div>
-
-        <div class="row">
-            <div class="col-md-4">
-                <div id="search_input">
-                    <div class="input-group col-md-12 ">
-                        <input type="text" class="form-control input-mg" v-bind:placeholder=appendString v-model="search"/>
-                        <span class="input-group-btn">
-                            <button class="btn btn-primary btn-mg" type="button">
-                                <i class="fa fa-search" aria-hidden="true"></i>
-                            </button>
-                        </span>
-                    </div>
-                </div>
-            </div>
-           
-
-            <div class="col-md-1 " >
-                <span class="input-group-btn">
-                            <button type="button" class="btn btn-primary btn-sm" id="addButton" v-on:click="add">
-                                <i class="fa fa-plus" aria-hidden="true"></i>
-                            </button>
-                </span>
-            </div>
-        </div>
     
 
-    <div class="content" v-show=loading>
-        <div class="row">
-            <div class="col-md-8" col-md-offset-6>
-                
-                <div class="list-group">
-                  <li v-for="user in searchId" class="list-group-item">
-                    {{ user.firstName }} {{ user.lastName }} {{ user.email }}
-                    <span class="pull-right">
-                      <button type="button" class="btn btn-primary btn-xs btn-primary" v-on:click="edit(user.id)" id="editButton">
-                        <i class="fa fa-pencil" aria-hidden="true"></i>
-                      </button>
-                      <button type="button" class="btn btn-primary btn-xs btn-danger" v-on:click="remove(user.id)" id="removeButton">
-                        <i class="fa fa-trash" aria-hidden="true"></i>
-                      </button>
-                    </span>
-                  </li>
-                </div>
+        <info-pane v-for="users in userList"
+                    :textValues="new Array(users.name)"
+                   :remove="remove"
+                   :objectId= "users.id"
+                   edit="edit_user"
+                   show="user"
+                   :key="users.id">
+        </info-pane>
 
-            </div>
-        </div>
-
-    </div>
-
-    <!-- Temp button because server is down -->
-    <button type="button" class="btn btn-primary btn-xs btn-primary" v-on:click="edit(123)" id="editButton">
-                        <i class="fa fa-pencil" aria-hidden="true"></i>
-                      </button>
-                      <button type="button" class="btn btn-primary btn-xs btn-danger" v-on:click="remove(user.id)" id="removeButton">
-                        <i class="fa fa-trash" aria-hidden="true"></i>
-    </button>
+        <button type="button" class="btn btn-primary btn-circle btn-lg" v-on:click="add()">+</button>
 
 </div>
 
@@ -66,43 +22,37 @@
 
 </template>
 <script>
+import infoPane from "../../assets/listComponent.vue"
     export default {
+        components: {
+            'info-pane': infoPane
+        },
         data(){
         return {
-            identityType: this.$route.query.type + ' zoeken',
-            users: [],
-            clients:[],
-            search: '',
-            loading: false
+            userList : [ 
+                    {id: 1, name: 'User1'},
+                    {id: 2, name: 'User2'},
+                    {id: 3, name: 'User3'},
+                    {id: 4, name: 'User4'}
+                ],
+            users:{},
+            search: ''
         }
         },
         methods:{
                 // Methods for routing purposes 
                 add: function () {
-                        this.$router.push({ path: 'users/new', query: { type: this.$route.query.type  }})
-                },
-                edit : function (id){
-                    alert( ' Edit ' + id)
-                    // API CALL DIE GEGEVENS OPVRAAGT van user en springt naar edit page.
-                    this.$router.push({ path: '/users/:id/edit', query: { type: this.$route.query.type, id: id }})
+                        this.$router.push({ path: 'users/new'})
                 },
                 remove : function (id){
-                     confirm("Wil u doorgaan met het verwijderen?")
+                     confirm("Wil u doorgaan met het verwijderen van?"+id)
                    
                     // API CALL VOOR VERWIJDEREN 
-
-                    if(this.$route.query.type == "Gebruiker"){
                         console.log('https://vopro5.ugent.be/app/api/users/'+id)
-                        this.$http.delete('https://vopro5.ugent.be/app/api/users/'+id).then(response => {
+/*                        this.$http.delete('https://vopro5.ugent.be/app/api/users/'+id).then(response => {
                                 
                         })
-                    }
-                    else if(this.$route.query.type == "Klant"){
-                        console.log('https://vopro5.ugent.be/app/api/companies/'+id)
-                        this.$http.delete('https://vopro5.ugent.be/app/api/companies/'+id).then(response => {
-                                
-                        })
-                    }
+                    */
 
                     // remove user in table
                     // var i = this.users.indexOf(user);
@@ -112,45 +62,51 @@
                 },
         },
         computed: {
-        searchId: function () { 
-            var s=this.search.trim().toLowerCase();;
-            var listID = []
-
-
-            if (this.$route.query.type == "Gebruiker"){
+            searchId: function () { 
+                var s=this.search.trim().toLowerCase();;
+                var listID = []
                 listID = this.users
-            }
-            else if (this.$route.query.type == "Klant"){
-                listID = this.clients
-            }
-            if(!this.search){
-                return listID
-            }
-            else{
-                console.log(listID)
-                return listID.filter(function(number){
-                    if(number.firstName.toLowerCase().indexOf(s) != -1 || number.lastName.toLowerCase().indexOf(s) != -1 || number.email.toLowerCase().indexOf(s) != -1){
-                        return number
-                    }
-                })
-            }
-
-        },
-        appendString : function (){
-            var value = 'Gebruiker zoeken'
-            return value
-        },
+                if(!this.search){
+                    return listID
+                }
+                else{
+                    console.log(listID)
+                    return listID.filter(function(number){
+                        if(number.name.toLowerCase().indexOf(s) != -1){
+                            return number
+                        }
+                    })
+                }
+            },
+            appendString : function (){
+                var value = 'Gebruiker zoeken'
+                return value
+            },
         },
         mounted: function () {
             // Make ajax request to server according to type
+                console.log('Get User objects')
 
-                console.log('Get users object')
-                // GET user objects
-                this.$http.get('https://vopro5.ugent.be/app/api/users').then(response => {
-                    this.users = response.body
-                })
-            
-            this.loading = true
+/*                this.$http.get('https://vopro5.ugent.be/app/api/users').then(response => {
+                    console.log(response.body.data)
+                    console.log(this.userList)
+                    this.userList = response.body.data
+                    console.log(this.userList)
+                })*/
         },
     }
 </script>
+
+<style>
+    .btn-circle.btn-lg {
+        position: fixed;
+        left: 230px;
+        bottom: 40px;
+        width: 50px;
+        height: 50px;
+        padding: 10px 16px;
+        font-size: 18px;
+        line-height: 1.33;
+        border-radius: 25px;
+}
+</style>
