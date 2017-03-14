91ff4c7bcfb6ba4588286ecd0a09e90dbb9de07a
Sam Persoon <sam.persoon@ugent.be>
2017-03-14 15:02:33 +0100

    Fix production

--- app/backend/src/test/java/dao/database/ProductionVehicleDAOFiltersTest.java
+++ app/backend/src/test/java/dao/database/ProductionVehicleDAOFiltersTest.java
@@ -25,7 +25,7 @@ public class ProductionVehicleDAOFiltersTest {
     //Setup before any of the tests are started
     @BeforeClass
     public static void initProvider() throws Exception {
-        ProductionProvider.initializeProvider(false);
+        ProductionProvider.initializeProvider(true);
         daoProvider = (ProductionProvider) ProductionProvider.getInstance();
     }
 
--- app/backend/src/test/java/dao/database/ProductionVehicleDAOTest.java
+++ app/backend/src/test/java/dao/database/ProductionVehicleDAOTest.java
@@ -24,7 +24,7 @@ public class ProductionVehicleDAOTest {
     //Setup before any of the tests are started
     @BeforeClass
     public static void initProvider() throws Exception{
-        ProductionProvider.initializeProvider(false);
+        ProductionProvider.initializeProvider(true);
         daoProvider = (ProductionProvider) ProductionProvider.getInstance();
     }
 
