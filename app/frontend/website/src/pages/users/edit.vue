0de4fcdc012a66c0827c58e36c1b120035095e7b
fversche <Freek.Verschelden@UGent.be>
2017-03-14 15:01:34 +0100

    Adjust edit for users

--- app/frontend/website/src/pages/users/edit.vue
+++ app/frontend/website/src/pages/users/edit.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="content-wrapper">
      <div class="page-header">    
-     <h1> Gebruiker aanpassen</h1>
+     <h1> Gebruiker aanpassen ({{this.$route.params.id}}) </h1>
    </div>  
    <form-temp :at=this.data :type=this.type></form-temp>
 </div>
@@ -21,7 +21,9 @@
                 console.log('creating edit.vue')
                 // listen to proceed performed by child component
                 var vm = this
-
+                var id = this.$route.params.id
+                console.log(id)
+                
                 this.$bus.$on('proceedEdit', function(input){
                     console.log(input)
                     console.log('proceedEdit called')
@@ -29,20 +31,19 @@
                     console.log("Gebruiker aangepast")
                     // Make post request for user
                     console.log('https://vopro5.ugent.be/app/api/users/'+input)
-                    this.$http.put('https://vopro5.ugent.be/app/api/users/'+this.$route.query.id).then(response => {
+/*                    this.$http.put('https://vopro5.ugent.be/app/api/users/'+id).then(response => {
                         
-                    })
-                    vm.$router.go(-1)
+                    })  */
                 });
 
 
                 // get specific data to edit identity
         
-                console.log('https://vopro5.ugent.be/app/api/users/'+this.$route.query.id )
-                this.$http.get('https://vopro5.ugent.be/app/api/users/'+this.$route.query.id).then(response => {
+                console.log('https://vopro5.ugent.be/app/api/users/'+id )
+/*                this.$http.get('https://vopro5.ugent.be/app/api/users/'+id).then(response => {
                         // fire event that will send specific data to child component (Form)
                         this.$bus.$emit('getEditInfo',response.body,"Gebruiker")
-                })
+                })*/
             }
         }
 
