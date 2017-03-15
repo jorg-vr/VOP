2796152227236aa72827fc0dfec13a1ed4b997a2
tdpontie <thomas.depontieu@ugent.be>
2017-03-15 16:57:57 +0100

    Updates ProductionVehcileDAOFiltersTest again

--- app/backend/src/test/java/dao/database/ProductionVehicleDAOFiltersTest.java
+++ app/backend/src/test/java/dao/database/ProductionVehicleDAOFiltersTest.java
@@ -5,10 +5,11 @@ import model.fleet.Fleet;
 import model.fleet.Vehicle;
 import model.fleet.VehicleType;
 import model.identity.Customer;
-import org.junit.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Collection;
 
 import static org.junit.Assert.assertTrue;
@@ -20,9 +21,12 @@ public class ProductionVehicleDAOFiltersTest {
     private static DAOProvider daoProvider;
     private static VehicleDAO vehicleDAO;
     private static VehicleTypeDao vehicleTypeDAO;
+    private static FleetDAO fleetDAO;
+    private static CustomerDAO customerDAO;
     private static Vehicle v1, v2, v3;
     private static VehicleType t1, t2;
     private static Fleet fleet1, fleet2;
+    private static Customer cust1;
 
     //TODO: production to false, when local
     //Setup before any of the tests are started
@@ -32,14 +36,14 @@ public class ProductionVehicleDAOFiltersTest {
         daoProvider = ProductionProvider.getInstance();
         vehicleDAO = daoProvider.getVehicleDAO();
         vehicleTypeDAO = daoProvider.getVehicleTypeDAO();
-        FleetDAO fleetDAO = daoProvider.getFleetDAO();
-        CustomerDAO customerDAO = daoProvider.getCustomerDAO();
+        fleetDAO = daoProvider.getFleetDAO();
+        customerDAO = daoProvider.getCustomerDAO();
 
         t1 = vehicleTypeDAO.create("type 1", 2.5);
         t2 = vehicleTypeDAO.create("type 2", 5.7);
-        Customer cust1 = customerDAO.create("customer 1", null, "123", "456", new ArrayList<>());
-        fleet1 = fleetDAO.create("name 1", cust1, new ArrayList<>());
-        fleet1 = fleetDAO.create("name 1", cust1, new ArrayList<>());
+        cust1 = customerDAO.create("customer 1", null, "123", "456");
+        fleet1 = fleetDAO.create("name 1", cust1);
+        fleet1 = fleetDAO.create("name 1", cust1);
         v1 = vehicleDAO.create("brand 1", "model 1", "AAAAAAAAAAAAAAAAA", "ABC-123", 500, 3000, t1, LocalDate.of(2016, 7, 15), fleet1);
         v2 = vehicleDAO.create("brand 1", "model 2", "BBBBBBBBBBBBBBBBB", "DEF-123", 1000, 3500, t2, LocalDate.of(2016, 7, 26), fleet2);
         v3 = vehicleDAO.create("brand 2", "model 2", "CCCCCCCCCCCCCCCCC", "DEF-456", 1500, 4000, t1, LocalDate.of(2016, 9, 26), fleet2);
@@ -51,6 +55,9 @@ public class ProductionVehicleDAOFiltersTest {
         vehicleDAO.remove(v1.getUuid());
         vehicleDAO.remove(v2.getUuid());
         vehicleDAO.remove(v3.getUuid());
+        fleetDAO.remove(fleet1.getUuid());
+        fleetDAO.remove(fleet2.getUuid());
+        customerDAO.remove(cust1.getUuid());
         vehicleTypeDAO.remove(t1.getUuid());
         vehicleTypeDAO.remove(t2.getUuid());
         daoProvider.close();
@@ -118,7 +125,7 @@ public class ProductionVehicleDAOFiltersTest {
     public void maxMileage() throws Exception {
         Collection<Vehicle> c1 = vehicleDAO.listFiltered(vehicleDAO.maxMileage(3000));
         Collection<Vehicle> c2 = vehicleDAO.listFiltered(vehicleDAO.maxMileage(4500));
-        assertTrue("maxMileage filter werkt niet", c1.contains(v1)&& !c1.contains(v2) && !c1.contains(v3));
+        assertTrue("maxMileage filter werkt niet", c1.contains(v1) && !c1.contains(v2) && !c1.contains(v3));
         assertTrue("maxMileage filter werkt niet", c2.contains(v1) && c2.contains(v2) && c2.contains(v3));
     }
 
@@ -140,9 +147,9 @@ public class ProductionVehicleDAOFiltersTest {
 
     @Test
     public void multipleFilters() throws Exception {
-        Vehicle v4 = vehicleDAO.create("brand 1", "model 2", "FAAAAAAAAAAAAAAAA", "ABC-123", 500, 4000, t2, LocalDate.of(2016, 9, 28));
-        Vehicle v5 = vehicleDAO.create("brand 2", "model 1", "FBBBBBBBBBBBBBBBB", "DEF-123", 1000, 4500, t1, LocalDate.of(2017, 7, 26));
-        Vehicle v6 = vehicleDAO.create("brand 1", "model 2", "FCCCCCCCCCCCCCCCC", "DEF-456", 1500, 5000, t2, LocalDate.of(2017, 9, 26));
+        Vehicle v4 = vehicleDAO.create("brand 1", "model 2", "FAAAAAAAAAAAAAAAA", "ABC-123", 500, 4000, t2, LocalDate.of(2016, 9, 28), fleet2);
+        Vehicle v5 = vehicleDAO.create("brand 2", "model 1", "FBBBBBBBBBBBBBBBB", "DEF-123", 1000, 4500, t1, LocalDate.of(2017, 7, 26), fleet1);
+        Vehicle v6 = vehicleDAO.create("brand 1", "model 2", "FCCCCCCCCCCCCCCCC", "DEF-456", 1500, 5000, t2, LocalDate.of(2017, 9, 26), fleet1);
         Collection<Vehicle> c1 = vehicleDAO.listFiltered(vehicleDAO.byType(t1), vehicleDAO.byModel("model 1"));
         Collection<Vehicle> c2 = vehicleDAO.listFiltered(vehicleDAO.byBrand("brand 1"), vehicleDAO.byLicensePlate("DEF-123"));
         Collection<Vehicle> c3 = vehicleDAO.listFiltered(vehicleDAO.atLeastMileage(4000), vehicleDAO.beforeProductionDate(LocalDate.of(2016, 9, 28)));
