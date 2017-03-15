6e53ae5444ae436b9eb8ff455a9aa890d91abffb
Jorg Van Renterghem <jorg.vanrenterghem@ugent.be>
2017-03-15 18:52:34 +0100

    Implement restusercontrollertest

--- app/backend/src/test/java/spring/controller/RESTUserControllerTest.java
+++ app/backend/src/test/java/spring/controller/RESTUserControllerTest.java
@@ -1,11 +1,13 @@
 package spring.controller;
 
-import controller.*;
+import controller.AccountController;
+import controller.CustomerController;
+import controller.FunctionController;
+import controller.PersonController;
 import dao.database.ProductionProvider;
 import dao.interfaces.DataAccessException;
 import model.account.Account;
 import model.account.Function;
-import model.fleet.Fleet;
 import model.identity.Address;
 import model.identity.Customer;
 import model.identity.Person;
@@ -16,8 +18,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import spring.model.RESTFleet;
 import spring.model.RESTRole;
+import spring.model.RESTUser;
 
 import java.time.LocalDateTime;
 
@@ -31,24 +33,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * Created by jorg on 3/15/17.
  */
-public class RESTRoleControllerTest {
+public class RESTUserControllerTest {
     private MockMvc mvc= MockMvcBuilders.standaloneSetup(new RESTRoleController()).build();
 
-    private static Address address;
-    private static Customer customer;
     private static Person person;
     private static Account account;
-    private static Function function;
-
     @BeforeClass
     public static void setup() {
         ProductionProvider.initializeProvider(true);
         try {
-            address= new Address("mystreet","123","lala","12345","land");
-            customer= new CustomerController().create(address,"04789456123","anita","123456789");
             person=new PersonController().createPerson("jon","doe","jon.doe@hotmail.com");
             account=new AccountController().createAccount("jon.doe@hotmail.com","054561dfs5f465",person.getUuid());
-            function=new FunctionController().create(customer.getUuid(),"",account.getUuid(), LocalDateTime.now().minusMonths(12),LocalDateTime.now().plusMonths(12));
         } catch (DataAccessException e) {
             e.printStackTrace();
         }
@@ -56,10 +51,8 @@ public class RESTRoleControllerTest {
     @AfterClass
     public static void afterTransaction() {
         try {
-            new FunctionController().archive(function.getUuid());
             new AccountController().archive(account.getUuid());
             new PersonController().archive(person.getUuid());
-            new CustomerController().archive(customer.getUuid());
         } catch (DataAccessException e) {
             e.printStackTrace();
         }
@@ -69,7 +62,7 @@ public class RESTRoleControllerTest {
 
     @Test
     public void get() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/roles"))
+        mvc.perform(MockMvcRequestBuilders.get("/users"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath(".data",hasSize(greaterThanOrEqualTo(1))))
                 .andExpect(jsonPath("$.total",greaterThanOrEqualTo(1)))
@@ -79,52 +72,61 @@ public class RESTRoleControllerTest {
 
     @Test
     public void post() throws Exception {
-        RESTRole restRole=new RESTRole();
-        restRole.setCompanyId(UUIDUtil.UUIDToNumberString(customer.getUuid()));
-        restRole.setUserId(UUIDUtil.UUIDToNumberString(account.getUuid()));
+        RESTUser restUser=new RESTUser();
+        restUser.setEmail("Mymail@this.id");
+        restUser.setFirstName("Tim");
+        restUser.setLastName("Tom");
+        restUser.setPassword("password012345");
 
-        MvcResult result =mvc.perform(MockMvcRequestBuilders.post("/roles")
+        MvcResult result =mvc.perform(MockMvcRequestBuilders.post("/users")
                 .header("Content-Type","application/json")
-                .content(TestUtil.convertObjectToJsonBytes(restRole))
+                .content(TestUtil.convertObjectToJsonBytes(restUser))
         )
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.companyId",equalTo(restRole.getCompanyId())))
-                .andExpect(jsonPath("$.userId",equalTo(restRole.getUserId()))).andReturn();
+                .andExpect(jsonPath("$.email",equalTo(restUser.getEmail())))
+                .andExpect(jsonPath("$.lastName",equalTo(restUser.getLastName())))
+                .andExpect(jsonPath("$.firstName",equalTo(restUser.getFirstName())))
+                .andExpect(jsonPath("$.password",equalTo(restUser.getPassword())))
+                .andReturn();
         RESTRole restRole1 =  TestUtil.convertJsonBytesToObject(result.getResponse().getContentAsByteArray(),RESTRole.class);
-        mvc.perform(MockMvcRequestBuilders.delete("/roles/{id}",restRole1.getId()))
+        mvc.perform(MockMvcRequestBuilders.delete("/users/{id}",restRole1.getId()))
                 .andExpect(status().isOk());
-        mvc.perform(MockMvcRequestBuilders.get("/roles/{id}",restRole1.getId()))
+        mvc.perform(MockMvcRequestBuilders.get("/users/{id}",restRole1.getId()))
                 .andExpect(status().isNotFound());
     }
 
     @Test
     public void getId() throws Exception {
-        RESTRole restRole=new RESTRole();
-        restRole.setCompanyId(UUIDUtil.UUIDToNumberString(customer.getUuid()));
-        restRole.setUserId(UUIDUtil.UUIDToNumberString(account.getUuid()));
 
-        mvc.perform(MockMvcRequestBuilders.get("/roles/{id}",UUIDUtil.UUIDToNumberString(function.getUuid())))
+        mvc.perform(MockMvcRequestBuilders.get("/users/{id}",UUIDUtil.UUIDToNumberString(account.getUuid())))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.companyId",equalTo(restRole.getCompanyId())))
-                .andExpect(jsonPath("$.userId",equalTo(restRole.getUserId())))
+                .andExpect(jsonPath("$.id",equalTo(UUIDUtil.UUIDToNumberString(account.getUuid()))))
+                .andExpect(jsonPath("$.email",equalTo(person.getEmail())))
+                .andExpect(jsonPath("$.lastName",equalTo(person.getLastName())))
+                .andExpect(jsonPath("$.firstName",equalTo(person.getFirstName())))
+                .andExpect(jsonPath("$.password",equalTo(account.getHashedPassword())))
                 .andReturn();
     }
 
     @Test
     public void putId() throws Exception {
-        RESTRole restRole=new RESTRole();
-        restRole.setId(UUIDUtil.UUIDToNumberString( function.getUuid()));
-        restRole.setCompanyId(UUIDUtil.UUIDToNumberString(customer.getUuid()));
-        restRole.setFunction("");
-        restRole.setUserId(UUIDUtil.UUIDToNumberString(account.getUuid()));
+        RESTUser restUser=new RESTUser();
+        restUser.setId(UUIDUtil.UUIDToNumberString(account.getUuid()));
+        restUser.setEmail(person.getEmail());
+        restUser.setFirstName(person.getFirstName());
+        restUser.setLastName("Tom");
+        restUser.setPassword(account.getHashedPassword());
 
-        MvcResult result =mvc.perform(MockMvcRequestBuilders.put("/roles/{id}",UUIDUtil.UUIDToNumberString(function.getUuid()))
+        MvcResult result =mvc.perform(MockMvcRequestBuilders.put("/roles/{id}",UUIDUtil.UUIDToNumberString(account.getUuid()))
                 .header("Content-Type","application/json")
-                .content(TestUtil.convertObjectToJsonBytes(restRole))
+                .content(TestUtil.convertObjectToJsonBytes(restUser))
         )
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.companyId",equalTo(restRole.getCompanyId())))
-                .andExpect(jsonPath("$.userId",equalTo(restRole.getUserId())))
+                .andExpect(jsonPath("$.id",equalTo(restUser.getId())))
+                .andExpect(jsonPath("$.email",equalTo(restUser.getEmail())))
+                .andExpect(jsonPath("$.lastName",equalTo(restUser.getLastName())))
+                .andExpect(jsonPath("$.firstName",equalTo(restUser.getFirstName())))
+                .andExpect(jsonPath("$.password",equalTo(restUser.getPassword())))
                 .andReturn();
 
     }
