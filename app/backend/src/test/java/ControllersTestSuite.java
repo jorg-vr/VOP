51b44de028c689da0534a974421b26478af861be
tdpontie <thomas.depontieu@ugent.be>
2017-03-16 00:09:00 +0100

    Changes the testrunner to include controller tests

--- app/backend/src/test/java/ControllersTestSuite.java
+++ app/backend/src/test/java/ControllersTestSuite.java
@@ -1,13 +1,16 @@
-import dao.database.ProductionVehicleDAOTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import spring.controller.*;
 
 @RunWith(Suite.class)
 
 //Add Controller test classes whenever a new test class is made.
 @Suite.SuiteClasses({
-        //AccountControllerTest.class,
-        //CustomerControllerTest.class, etc...
+        RESTCompanyControllerTest.class,
+        RESTFleetControllerTest.class,
+        RESTRoleController.class,
+        RESTUserControllerTest.class,
+        UUIDUtilTest.class
 })
 
 public class ControllersTestSuite {
