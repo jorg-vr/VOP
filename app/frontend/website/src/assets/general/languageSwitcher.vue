76bd26a44b043e6b65ab98552f82cc17ac12a477
Jarre Knockaert <jarknock@ugent.be>
2017-03-27 16:15:35 +0200

    add language switcher

---
+++ app/frontend/website/src/assets/general/languageSwitcher.vue
@@ -0,0 +1,40 @@
+<!--
+    Based on https://gist.github.com/martinlindhe/f34489126eb635cb241f
+-->
+<template>
+    <select class="selectpicker" data-width="fit">
+        <option v-for="lang in languages" :class="'flag-icon flag-icon-' + lang.code">{{lang.name}}</option>
+    </select>
+</template>
+
+<script>
+    import Vue from 'vue';
+    export default {
+        data() {
+            return {
+                lang: Vue.config.lang,
+                languages: [
+                    {
+                        code: 'nl',
+                        name: 'Nederlands'
+                    },
+                    {
+                        code: 'en',
+                        name: 'English'
+                    }
+                ]
+            }
+        },
+        components: {
+            language: {
+                props: ['code', 'name'],
+                template: '<span @click="selectLanguage">{{ name }}</span>',
+                methods: {
+                    electLanguage(e) {
+                        Vue.config.lang = this.code;
+                    }
+                }
+            }
+        }
+    }
+</script>
--- app/frontend/website/src/assets/general/navBar.vue
+++ app/frontend/website/src/assets/general/navBar.vue
@@ -5,16 +5,22 @@
     <div id="sidebar-wrapper">
         <ul class="sidebar-nav">
             <li class="sidebar-brand">Solvas fleet</li>
-            <li><router-link :to="{name: 'fleets'}">Vloten</router-link></li>
-            <li><router-link :to="{name: 'users'}">Gebruikers</router-link></li>
-            <li><router-link :to="{name: 'clients'}">Klanten</router-link></li>
+            <li><router-link :to="{name: 'fleets'}">{{$t("fleet.fleets") | capitalize}}</router-link></li>
+            <li><router-link :to="{name: 'users'}">{{$t("user.users") | capitalize}}</router-link></li>
+            <li><router-link :to="{name: 'clients'}">{{$t("client.clients") | capitalize}}</router-link></li>
             <li><a href="#">About</a></li>
             <li><a href="#">Services</a></li>
             <li><a href="#">Contact</a></li>
         </ul>
+        <language-switcher></language-switcher>
     </div>
 </template>
 <script>
-    export default {}
+    import languageSwitcher from './languageSwitcher.vue'
+    export default {
+        components: {
+            languageSwitcher
+        }
+    }
 </script>
 
