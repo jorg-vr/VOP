800985151cd6a873529e121784929233c01ba368
Tomas Van Roose <tomas.vanroose@ugent.be>
2017-04-03 19:12:26 +0200

    Change main layout to fit new sidebar

--- app/frontend/website/src/app.vue
+++ app/frontend/website/src/app.vue
@@ -1,13 +1,32 @@
 <template>
-    <div id="page-content-wrapper">
+    <div id="wrapper">
         <nav-bar></nav-bar>
-        <div class="container-fluid">
-            <div class="row">
-                <router-view></router-view>
-            </div>
+        <div id="page-wrapper">
+            <router-view></router-view>
         </div>
     </div>
 </template>
+
+<style>
+#wrapper {
+    background-color: #2f4050;
+    min-height: 100vh;
+}
+
+#page-wrapper {
+    margin-left: 220px;
+    background-color: #f3f3f4;
+    padding: 0 15px 0 15px;
+    min-height: 100vh;
+}
+
+.content {
+    padding: 20px 20px 20px 20px;
+    background-color: white;
+}
+
+</style>
+
 <script>
 
     import NavBar from './assets/general/navBar.vue'
