24fbfca4c886c236a87d7ee64713cc5fd16dfa2b
Tomas Van Roose <tomas.vanroose@ugent.be>
2017-04-03 20:10:01 +0200

    Begin restyle navbar

--- app/frontend/website/src/assets/general/navBar.vue
+++ app/frontend/website/src/assets/general/navBar.vue
@@ -2,19 +2,23 @@
     Navigation bar of the website
 -->
 <template>
-    <div id="sidebar-wrapper">
-        <ul class="sidebar-nav">
-            <li class="sidebar-brand">Solvas fleet</li>
-            <li><router-link :to="{name: 'fleets'}">{{$t("fleet.fleets") | capitalize}}</router-link></li>
-            <li><router-link :to="{name: 'users'}">{{$t("user.users") | capitalize}}</router-link></li>
-            <li><router-link :to="{name: 'clients'}">{{$t("client.clients") | capitalize}}</router-link></li>
-            <li><router-link :to="{name: 'vehicles'}">{{$t("vehicle.vehicles") | capitalize}}</router-link></li>
-        </ul>
-        <language-switcher></language-switcher>
-    </div>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-static-side" role="navigation">
+        <div id="sidebar-collapse">
+            <ul class="nav">
+                <li class="sidebar-brand">Solvas fleet</li>
+                <li><router-link :to="{name: 'fleets'}">{{$t("fleet.fleets") | capitalize}}</router-link></li>
+                <li><router-link :to="{name: 'users'}">{{$t("user.users") | capitalize}}</router-link></li>
+                <li><router-link :to="{name: 'clients'}">{{$t("client.clients") | capitalize}}</router-link></li>
+                <li><router-link :to="{name: 'vehicles'}">{{$t("vehicle.vehicles") | capitalize}}</router-link></li>
+            </ul>
+            <language-switcher></language-switcher>
+        </div>
+    </nav>
 </template>
 <script>
-    import languageSwitcher from './languagePicker.vue'
+import languageSwitcher from './languagePicker.vue'
     export default {
         components: {
             languageSwitcher
@@ -22,3 +26,44 @@
     }
 </script>
 
+<style lang = "less">
+navbar {
+  border: 0;
+  z-index: 2000;
+}
+.navbar-default {
+  background-color :#2f4050;
+}
+.navbar-top-links li {
+  display: inline-block;
+}
+.navbar-top-links li:last-child {
+  margin-right: 40px;
+}
+.nav > li > a {
+  color: #a7b1c2;
+  font-weight: 600;
+  padding: 14px 20px 14px 25px;
+}
+.nav > li.active > a {
+  color: #ffffff;
+}
+.navbar-default .nav > li > a:hover,
+.navbar-default .nav > li > a:focus {
+  background-color: #293846;
+  color: white;
+}
+.navbar-static-side {
+  width: 220px;
+  position: absolute;
+}
+.nav-header {
+    padding-top: 33px;
+    padding-bottom: 25px;
+    box-sizing: content-box;
+    height: 100px;
+}
+hr {
+    margin-top: 0;
+}
+</style>
