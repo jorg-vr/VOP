641eb5080436742a0669836a1f67d16181636e2d
fversche <Freek.Verschelden@UGent.be>
2017-04-04 23:27:11 +0200

    Adjust app component for login

--- app/frontend/website/src/app.vue
+++ app/frontend/website/src/app.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="page-content-wrapper">
-        <nav-bar></nav-bar>
+        <nav-bar v-show="showNavBar"></nav-bar>
+        <!-- login screen as modal -->
+        <login-modal @confirmLogin="switchLayout()" v-show="showLogin"></login-modal>
         <div class="container-fluid">
             <div class="row">
                 <router-view></router-view>
@@ -9,11 +11,28 @@
     </div>
 </template>
 <script>
-
+    import { mapGetters } from 'vuex'
     import NavBar from './assets/general/navBar.vue'
+    import LoginModal from './assets/general/loginModal.vue'
     export default {
         components: {
-            NavBar
-        }
+            NavBar,
+            LoginModal
+        },
+        data() {
+            return {
+                showNavBar : false, 
+            }
+        },
+        methods: {
+            switchLayout:function(id){
+                this.showNavBar=true
+            }
+        },
+         computed: {
+            ...mapGetters([
+                'showLogin'
+            ])
+        },
     }
 </script>
