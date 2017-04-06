da8274023341ab11f3f4b46b9578daeaa515d239
tdpontie <thomas.depontieu@ugent.be>
2017-04-06 21:46:28 +0200

    Adds test for Customer's fleets collection

--- app/backend/src/test/java/database/DataStructuresTest.java
+++ app/backend/src/test/java/database/DataStructuresTest.java
@@ -36,7 +36,7 @@ public class DataStructuresTest {
     public static void initProvider() throws Exception {
         daoProvider = ProductionProvider.getInstance();
         if(daoProvider == null){
-            ProductionProvider.initializeProvider("localtest");
+            ProductionProvider.initializeProvider("test");
             daoProvider = ProductionProvider.getInstance();
             notLocalTest = true;
         }
@@ -55,6 +55,12 @@ public class DataStructuresTest {
         }
     }
 
+    //TODO: test if duplicate vehicles can be added to a fleet
+    /**
+     * tests if the link between vehicles and their fleet is correctly maintained at all times.
+     * @throws Exception
+     */
+    @Ignore
     @Test
     public void vehiclesCollection() throws Exception {
         Address a1 = addressDAO.create(new Address("streettest n1", "59", "town 1", "9999", "country 1"));
@@ -122,7 +128,15 @@ public class DataStructuresTest {
     @Ignore
     @Test
     public void functionsCollection() throws Exception {
-        //TODO
+        //TODO: make function and user DAO tests first before starting this
+
+
+
+
+
+
+
+
     }
 
     /**
@@ -136,10 +150,35 @@ public class DataStructuresTest {
         //TODO
     }
 
+    /**
+     * the owner field is required for customers so less tests are required compared to vehicles
+     * @throws Exception
+     */
     @Ignore
     @Test
     public void fleetsCollection() throws Exception {
-        //TODO
+        Address a1 = addressDAO.create(new Address("streettest n1", "59", "town 1", "9999", "country 1"));
+        Customer c1 = customerDAO.create(new Customer(a1, "Email@address1.com", "911", "customername 1", "btw123", "123456789", CompanyType.TYPE1));
+
+        //Test if a fleet is automatically added to the owner's fleets collection upon creation
+        Fleet fleet1 = fleetDAO.create(new Fleet("fleet 1", c1));
+        assertTrue("fleet was not automatically added to the owner's fleets collection", customerDAO.get(c1.getUuid()).getFleets().contains(fleet1));
+
+        //Test if fleets are automatically removed from the customer's fleets collection when a fleet is removed from the database
+        if(!customerDAO.get(c1.getUuid()).getFleets().contains(fleet1)){
+            c1 = customerDAO.get(c1.getUuid());
+            c1.addFleet(fleet1);
+            customerDAO.update(c1);
+        }
+        fleetDAO.remove(fleet1.getUuid());
+        c1 = customerDAO.get(c1.getUuid());
+        assertTrue("fleet still remains in the customer's fleets collection after removing it from the database", c1.getFleets().contains(fleet1));
+
+        //TODO: potentially tests on removing fleets from the fleets collection instead of directly from the database
+
+        //cleaning up the database
+        customerDAO.remove(c1.getUuid());
+        addressDAO.remove(a1.getUuid());
     }
 
     @Ignore
