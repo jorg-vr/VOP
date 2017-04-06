8a6ae387a24f9f4c9f4db7987dd0f9f50fc40c15
Jarre Knockaert <jarre.knockaert@ugent.be>
2017-04-06 20:29:01 +0200

    implement RESTFunctionController

--- app/backend/src/main/java/spring/controller/RESTFunctionController.java
+++ app/backend/src/main/java/spring/controller/RESTFunctionController.java
@@ -1,11 +1,25 @@
 package spring.controller;
 
 import controller.ControllerFactory;
+import controller.CustomerController;
+import controller.FleetController;
+import controller.FunctionController;
+import controller.exceptions.UnAuthorizedException;
+import dao.interfaces.DataAccessException;
 import model.account.Function;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import model.fleet.Fleet;
+import org.springframework.web.bind.annotation.*;
+import spring.exceptions.InvalidInputException;
+import spring.exceptions.NotAuthorizedException;
+import spring.model.RESTFleet;
 import spring.model.RESTFunction;
 import spring.model.RESTModelFactory;
+import spring.model.RESTSchema;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
 
 /**
  * Created by Jarre on 6-4-2017.
@@ -15,7 +29,36 @@ import spring.model.RESTModelFactory;
 @RequestMapping("/users/{userId}/functions")
 public class RESTFunctionController extends RESTAbstractController<RESTFunction, Function> {
 
-    public RESTFunctionController(ControllerFactory<Function> controllerFactory, RESTModelFactory<RESTFunction, Function> factory) {
-        super(controllerFactory, factory);
+    public RESTFunctionController() {
+        super(FunctionController::new, RESTFunction::new);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public RESTSchema<RESTFunction> get(HttpServletRequest request,
+                                        @PathVariable Optional<String> userId,
+                                        @RequestParam(required = false) String user,
+                                        @RequestParam(required = false) String company,
+                                        @RequestParam(required = false) String role,
+                                        @RequestParam(required = false) Integer page,
+                                        @RequestParam(required = false) Integer limit,
+                                        @RequestParam(required = false) String sort,
+                                        @RequestHeader(value="AuthToken") String token,
+                                        @RequestHeader(value="Function") String authorityFunction){
+
+        if(userId.isPresent()){
+            user = userId.get();
+        }
+        Collection<RESTFunction> restFunctions = new ArrayList<>();
+        try(FunctionController controller = new FunctionController(verifyToken(token, authorityFunction))){
+            for(Function function: controller.getAll()){
+                restFunctions.add(new RESTFunction(function));
+            }
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        } catch (UnAuthorizedException e) {
+            throw new NotAuthorizedException();
+        }
+
+        return new RESTSchema<RESTFunction>(restFunctions, page, limit, request);
     }
 }
