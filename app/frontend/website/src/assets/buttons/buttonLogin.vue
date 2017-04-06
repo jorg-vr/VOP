30481f1c05cb5380b8ba2533856ff66b5e9b0895
fversche <Freek.Verschelden@UGent.be>
2017-04-06 20:35:38 +0200

    Add login button component

---
+++ app/frontend/website/src/assets/buttons/buttonLogin.vue
@@ -0,0 +1,19 @@
+<template>
+    <button type="button" :class="buttonClass" @click="emitClicked">
+        <slot></slot>
+    </button>
+</template>
+<script>
+    export default {
+        props: {
+            action: Function,
+            buttonClass: String
+        },
+        methods: {
+            emitClicked(){
+                this.$emit('click')
+            }
+        }
+    }
+</script>
+
