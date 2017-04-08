8712b84cd51b95fb1edac39183ac1792083cc0a0
bwdvolde <billie.devolder@ugent.be>
2017-04-08 20:00:21 +0200

    Implement properties

---
+++ app/backend/src/main/java/util/MyProperties.java
@@ -0,0 +1,45 @@
+package util;
+
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+@Configuration
+public class MyProperties {
+
+    public static final String PATH_AUTH = "path.auth";
+    public static final String PATH_LOGIN = "path.login";
+    public static final String PATH_REFRESH = "path.refresh";
+
+    public static final String PATH_USERS = "path.users";
+    public static final String PATH_ROLES = "path.roles";
+    public static final String PATH_FUNCTIONS = "path.functions";
+    public static final String PATH_PERMISSIONS = "path.permissions";
+
+    public static final String PATH_FLEETS = "path.fleets";
+    public static final String PATH_VEHICLES = "path.vehicles";
+    public static final String PATH_TYPES = "path.types";
+
+    public static final String PATH_COMPANIES = "path.companies";
+
+
+    private static final String APPLICATION_PROPERTIES = "src/main/resources/application.properties";
+
+    private static Properties properties;
+
+    static {
+        properties = new Properties();
+        try (FileInputStream in = new FileInputStream(APPLICATION_PROPERTIES)) {
+            properties.load(in);
+        } catch (IOException e) {
+            System.err.println("Could not read properties file: " + APPLICATION_PROPERTIES);
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String property) {
+        return properties.getProperty(property);
+    }
+}
