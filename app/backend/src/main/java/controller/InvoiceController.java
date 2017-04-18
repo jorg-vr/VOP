74e77fab8c777918461ecbdbe346034bbb3f75cf
Jorg Van Renterghem <jorg.vanrenterghem@ugent.be>
2017-04-18 18:52:50 +0200

    Get all invoices

--- app/backend/src/main/java/controller/InvoiceController.java
+++ app/backend/src/main/java/controller/InvoiceController.java
@@ -1,19 +1,46 @@
 package controller;
 
+import controller.exceptions.UnAuthorizedException;
 import dao.database.ProductionProvider;
 import dao.interfaces.DAO;
+import dao.interfaces.DataAccessException;
+import dao.interfaces.Filter;
 import dao.interfaces.InvoiceDAO;
 import model.account.Function;
 import model.account.Resource;
+import model.account.User;
 import model.billing.Invoice;
+import model.identity.Company;
+import spring.exceptions.NotImplementedException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by jorg on 4/18/17.
  */
 public class InvoiceController extends AbstractController<Invoice> {
 
+
     public InvoiceController(Function function) {
         super(ProductionProvider.getInstance().getInvoiceDao(), Resource.BILLING, function);
+    }
+
+    public Collection<Invoice> getFiltered(Company company) throws UnAuthorizedException {
+        try {
+            List<Filter<Invoice>> filters = new ArrayList<>();
+            InvoiceDAO invoiceDAO=(InvoiceDAO) getDao();
+
+            if (company != null) {
+                filters.add(invoiceDAO.byPayer(company));
+            }
+
+            return getAll(filters.toArray(new Filter[filters.size()]));
+        } catch (DataAccessException e) {
+            // return empty list
+            return new ArrayList<>();
+        }
     }
 
     @Override
--- app/backend/src/main/java/spring/controller/RESTInvoiceController.java
+++ app/backend/src/main/java/spring/controller/RESTInvoiceController.java
@@ -1,16 +1,25 @@
 package spring.controller;
 
 import controller.ControllerFactory;
+import controller.CustomerController;
 import controller.InvoiceController;
+import controller.exceptions.UnAuthorizedException;
+import dao.interfaces.DataAccessException;
+import dao.interfaces.Filter;
+import dao.interfaces.InvoiceDAO;
 import model.billing.Invoice;
+import model.identity.Company;
 import org.springframework.web.bind.annotation.*;
+import spring.exceptions.NotAuthorizedException;
 import spring.model.RESTInvoice;
 import spring.model.RESTModelFactory;
 import spring.model.RESTSchema;
+import util.UUIDUtil;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by Billie Devolder on 17/04/2017.
@@ -25,30 +34,29 @@ public class RESTInvoiceController extends RESTAbstractController<RESTInvoice,In
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public RESTSchema<RESTInvoice> getAll(@PathVariable String fleetId,
+    public RESTSchema<RESTInvoice> getAll(@PathVariable String companyId,
                                           HttpServletRequest request,
                                           Integer page, Integer limit,
                                           @RequestHeader(value = "Authorization") String token,
                                           @RequestHeader(value = "Function") String authorityFunction) {
-        Collection<RESTInvoice> invoices = new ArrayList<>();
-        invoices.add(new RESTInvoice());
-        return new RESTSchema<>(invoices, page, limit, request);
+        try(InvoiceController controller=new InvoiceController(verifyToken(token,authorityFunction));
+            CustomerController customerController=new CustomerController(verifyToken(token,authorityFunction))) {
+
+            Collection<RESTInvoice> invoices=new ArrayList<>();
+            Company company=customerController.get(UUIDUtil.toUUID(companyId));//TODO companycontroller
+            for(Invoice invoice:controller.getFiltered(company)){
+                invoices.add(new RESTInvoice(invoice));
+            }
+            return new RESTSchema<>(invoices, page, limit, request);
+        } catch (UnAuthorizedException e) {
+            throw new NotAuthorizedException();
+        } catch (DataAccessException e) {
+            throw  new RuntimeException(e);
+        }
+
+
     }
 
-    @RequestMapping(value = "{invoiceId}", method = RequestMethod.GET)
-    public RESTInvoice get(@PathVariable String fleetId,
-                           @PathVariable String invoiceId,
-                           @RequestHeader(value = "Authorization") String token,
-                           @RequestHeader(value = "Function") String authorityFunction) {
-        return new RESTInvoice();
-    }
-
-    @RequestMapping(value = "${path.invoices_current}", method = RequestMethod.GET)
-    public RESTInvoice getCurrent(@PathVariable String fleetId,
-                                  @RequestHeader(value = "Authorization") String token,
-                                  @RequestHeader(value = "Function") String authorityFunction) {
-        return new RESTInvoice();
-    }
 
 
 }
