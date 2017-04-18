10d05a013e341ed3716209540e8eb2b69899750f
tdpontie <Thomas.DePontieu@UGent.be>
2017-04-18 13:04:59 +0200

    All DAO tests use try-with-resources

--- app/backend/src/test/java/dao/database/ProductionAddressDAOFiltersTest.java
+++ app/backend/src/test/java/dao/database/ProductionAddressDAOFiltersTest.java
@@ -15,7 +15,6 @@ import static org.junit.Assert.assertTrue;
 
 public class ProductionAddressDAOFiltersTest {
     private static DAOProvider daoProvider;
-    private static AddressDAO addressDAO;
     private static Address a1, a2, a3;
 
     //Setup before any of the tests are started
@@ -23,61 +22,73 @@ public class ProductionAddressDAOFiltersTest {
     public static void initProvider() throws Exception {
         ProductionProvider.initializeProvider("unittest");
         daoProvider = ProductionProvider.getInstance();
-        addressDAO = daoProvider.getAddressDao();
 
-        a1 = addressDAO.create(new Address("streettest n1", "59", "town 1", "9999", "country 1"));
-        a2 = addressDAO.create(new Address("streettest n2", "59", "town 2", "9999", "country 2"));
-        a3 = addressDAO.create(new Address("streettest n2", "60", "town 2", "99999", "country 2"));
+        try (AddressDAO addressDAO = daoProvider.getAddressDao();) {
+            a1 = addressDAO.create(new Address("streettest n1", "59", "town 1", "9999", "country 1"));
+            a2 = addressDAO.create(new Address("streettest n2", "59", "town 2", "9999", "country 2"));
+            a3 = addressDAO.create(new Address("streettest n2", "60", "town 2", "99999", "country 2"));
+        }
     }
 
     //Gets executed after all tests have been run
     @AfterClass
     public static void closeProvider() throws Exception {
-        addressDAO.remove(a1.getUuid());
-        addressDAO.remove(a2.getUuid());
-        addressDAO.remove(a3.getUuid());
-        addressDAO.close();
+        try (AddressDAO addressDAO = daoProvider.getAddressDao();) {
+            addressDAO.remove(a1.getUuid());
+            addressDAO.remove(a2.getUuid());
+            addressDAO.remove(a3.getUuid());
+        }
         daoProvider.close();
     }
 
     @Test
     public void byStreet() throws Exception {
-        Collection<Address> c1 = addressDAO.listFiltered(addressDAO.byStreet("streettest n1"));
-        Collection<Address> c2 = addressDAO.listFiltered(addressDAO.byStreet("streettest n2"));
-        assertTrue("byStreet filter doesn't work", c1.contains(a1) && !c1.contains(a2) && !c1.contains(a3));
-        assertTrue("byStreet filter doesn't work", !c2.contains(a1) && c2.contains(a2) && c2.contains(a3));
+        try (AddressDAO addressDAO = daoProvider.getAddressDao();) {
+            Collection<Address> c1 = addressDAO.listFiltered(addressDAO.byStreet("streettest n1"));
+            Collection<Address> c2 = addressDAO.listFiltered(addressDAO.byStreet("streettest n2"));
+            assertTrue("byStreet filter doesn't work", c1.contains(a1) && !c1.contains(a2) && !c1.contains(a3));
+            assertTrue("byStreet filter doesn't work", !c2.contains(a1) && c2.contains(a2) && c2.contains(a3));
+        }
     }
 
     @Test
     public void byStreetNumber() throws Exception {
-        Collection<Address> c1 = addressDAO.listFiltered(addressDAO.byStreetNumber("59"));
-        Collection<Address> c2 = addressDAO.listFiltered(addressDAO.byStreetNumber("60"));
-        assertTrue("byStreetNumber filter doesn't work", c1.contains(a1) && c1.contains(a2) && !c1.contains(a3));
-        assertTrue("byStreetNumber filter doesn't work", !c2.contains(a1) && !c2.contains(a2) && c2.contains(a3));
+        try (AddressDAO addressDAO = daoProvider.getAddressDao();) {
+            Collection<Address> c1 = addressDAO.listFiltered(addressDAO.byStreetNumber("59"));
+            Collection<Address> c2 = addressDAO.listFiltered(addressDAO.byStreetNumber("60"));
+            assertTrue("byStreetNumber filter doesn't work", c1.contains(a1) && c1.contains(a2) && !c1.contains(a3));
+            assertTrue("byStreetNumber filter doesn't work", !c2.contains(a1) && !c2.contains(a2) && c2.contains(a3));
+        }
     }
 
     @Test
     public void byTown() throws Exception {
-        Collection<Address> c1 = addressDAO.listFiltered(addressDAO.byTown("town 1"));
-        Collection<Address> c2 = addressDAO.listFiltered(addressDAO.byTown("town 2"));
-        assertTrue("byTown filter doesn't work", c1.contains(a1) && !c1.contains(a2) && !c1.contains(a3));
-        assertTrue("byTown filter doesn't work", !c2.contains(a1) && c2.contains(a2) && c2.contains(a3));
+        try (AddressDAO addressDAO = daoProvider.getAddressDao();) {
+            Collection<Address> c1 = addressDAO.listFiltered(addressDAO.byTown("town 1"));
+            Collection<Address> c2 = addressDAO.listFiltered(addressDAO.byTown("town 2"));
+            assertTrue("byTown filter doesn't work", c1.contains(a1) && !c1.contains(a2) && !c1.contains(a3));
+            assertTrue("byTown filter doesn't work", !c2.contains(a1) && c2.contains(a2) && c2.contains(a3));
+        }
     }
 
     @Test
     public void byPostalCode() throws Exception {
-        Collection<Address> c1 = addressDAO.listFiltered(addressDAO.byPostalCode("9999"));
-        Collection<Address> c2 = addressDAO.listFiltered(addressDAO.byPostalCode("99999"));
-        assertTrue("byPostalCode filter doesn't work", c1.contains(a1) && c1.contains(a2) && c1.contains(a3));
-        assertTrue("byPostalCode filter doesn't work", !c2.contains(a1) && !c2.contains(a2) && c2.contains(a3));
+        try (AddressDAO addressDAO = daoProvider.getAddressDao();) {
+            Collection<Address> c1 = addressDAO.listFiltered(addressDAO.byPostalCode("9999"));
+            Collection<Address> c2 = addressDAO.listFiltered(addressDAO.byPostalCode("99999"));
+            assertTrue("byPostalCode filter doesn't work", c1.contains(a1) && c1.contains(a2) && c1.contains(a3));
+            assertTrue("byPostalCode filter doesn't work", !c2.contains(a1) && !c2.contains(a2) && c2.contains(a3));
+        }
     }
 
     @Test
     public void byCountry() throws Exception {
-        Collection<Address> c1 = addressDAO.listFiltered(addressDAO.byCountry("country 1"));
-        Collection<Address> c2 = addressDAO.listFiltered(addressDAO.byCountry("country 2"));
-        assertTrue("byCountry filter doesn't work", c1.contains(a1) && !c1.contains(a2) && !c1.contains(a3));
-        assertTrue("byCountry filter doesn't work", !c2.contains(a1) && c2.contains(a2) && c2.contains(a3));
+        try (AddressDAO addressDAO = daoProvider.getAddressDao();) {
+            Collection<Address> c1 = addressDAO.listFiltered(addressDAO.byCountry("country 1"));
+            Collection<Address> c2 = addressDAO.listFiltered(addressDAO.byCountry("country 2"));
+            assertTrue("byCountry filter doesn't work", c1.contains(a1) && !c1.contains(a2) && !c1.contains(a3));
+            assertTrue("byCountry filter doesn't work", !c2.contains(a1) && c2.contains(a2) && c2.contains(a3));
+        }
     }
 
     //TODO: test multiple filters in 1 request
--- app/backend/src/test/java/dao/database/ProductionVehicleTypeDAOFiltersTest.java
+++ app/backend/src/test/java/dao/database/ProductionVehicleTypeDAOFiltersTest.java
@@ -12,7 +12,6 @@ import static org.junit.Assert.assertTrue;
 
 public class ProductionVehicleTypeDAOFiltersTest {
     private static DAOProvider daoProvider;
-    private static VehicleTypeDAO vehicleTypeDAO;
     private static VehicleType t1, t2;
 
     //Setup before any of the tests are started
@@ -20,40 +19,46 @@ public class ProductionVehicleTypeDAOFiltersTest {
     public static void initProvider() throws Exception {
         ProductionProvider.initializeProvider("unittest");
         daoProvider = ProductionProvider.getInstance();
-        vehicleTypeDAO = daoProvider.getVehicleTypeDAO();
-        t1 = vehicleTypeDAO.create(new VehicleType("type 1", 2.5));
-        t2 = vehicleTypeDAO.create(new VehicleType("type 2", 5.7));
+        try (VehicleTypeDAO vehicleTypeDAO = daoProvider.getVehicleTypeDAO();) {
+            t1 = vehicleTypeDAO.create(new VehicleType("type 1", 2.5));
+            t2 = vehicleTypeDAO.create(new VehicleType("type 2", 5.7));
+        }
     }
 
     //Gets executed after all tests have been run
     @AfterClass
     public static void closeProvider() throws Exception {
-        vehicleTypeDAO.remove(t1.getUuid());
-        vehicleTypeDAO.remove(t2.getUuid());
-        vehicleTypeDAO.close();
+        try (VehicleTypeDAO vehicleTypeDAO = daoProvider.getVehicleTypeDAO();) {
+            vehicleTypeDAO.remove(t1.getUuid());
+            vehicleTypeDAO.remove(t2.getUuid());
+        }
         daoProvider.close();
     }
 
 
     @Test
     public void byName() throws Exception {
-        Collection<VehicleType> c1 = vehicleTypeDAO.listFiltered(vehicleTypeDAO.byName("type 1"));
-        Collection<VehicleType> c2 = vehicleTypeDAO.listFiltered(vehicleTypeDAO.byName("type 2"));
-        assertTrue("byName filter werkt niet", c1.contains(t1) && !c1.contains(t2));
-        assertTrue("byName filter werkt niet", !c2.contains(t1) && c2.contains(t2));
+        try (VehicleTypeDAO vehicleTypeDAO = daoProvider.getVehicleTypeDAO();) {
+            Collection<VehicleType> c1 = vehicleTypeDAO.listFiltered(vehicleTypeDAO.byName("type 1"));
+            Collection<VehicleType> c2 = vehicleTypeDAO.listFiltered(vehicleTypeDAO.byName("type 2"));
+            assertTrue("byName filter werkt niet", c1.contains(t1) && !c1.contains(t2));
+            assertTrue("byName filter werkt niet", !c2.contains(t1) && c2.contains(t2));
+        }
     }
 
     @Test
     public void nameContains() throws Exception {
-        Collection<VehicleType> c1 = vehicleTypeDAO.listFiltered(vehicleTypeDAO.nameContains("pe "));
-        Collection<VehicleType> c2 = vehicleTypeDAO.listFiltered(vehicleTypeDAO.nameContains("2"));
-        assertTrue("nameContains('pe ') filter does not work", c1.contains(t1) && c1.contains(t2));
-        for (VehicleType type : c1) {
-            assertTrue("nameContains('pe ') filter regex test does not pas", type.getType().matches(".*pe .*"));
-        }
-        assertTrue("nameContains('2') filter does not work", c2.contains(t2));
-        for (VehicleType type : c2) {
-            assertTrue("nameContains('2') filter regex test does not pas", type.getType().matches(".*2.*"));
+        try (VehicleTypeDAO vehicleTypeDAO = daoProvider.getVehicleTypeDAO();) {
+            Collection<VehicleType> c1 = vehicleTypeDAO.listFiltered(vehicleTypeDAO.nameContains("pe "));
+            Collection<VehicleType> c2 = vehicleTypeDAO.listFiltered(vehicleTypeDAO.nameContains("2"));
+            assertTrue("nameContains('pe ') filter does not work", c1.contains(t1) && c1.contains(t2));
+            for (VehicleType type : c1) {
+                assertTrue("nameContains('pe ') filter regex test does not pas", type.getType().matches(".*pe .*"));
+            }
+            assertTrue("nameContains('2') filter does not work", c2.contains(t2));
+            for (VehicleType type : c2) {
+                assertTrue("nameContains('2') filter regex test does not pas", type.getType().matches(".*2.*"));
+            }
         }
     }
 
