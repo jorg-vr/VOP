679b556c81eee086bd2b6d303322f68f6c294096
Sam Persoon <sam.persoon@ugent.be>
2017-04-30 14:49:32 +0200

    Update ProductionFunctionDAOTest

--- app/backend/src/test/java/dao/database/ProductionFunctionDAOTest.java
+++ app/backend/src/test/java/dao/database/ProductionFunctionDAOTest.java
@@ -17,19 +17,22 @@ import static org.junit.Assert.*;
 
 public class ProductionFunctionDAOTest {
     private static DAOManager daoManager;
+    private static DAOProvider daoProvider;
 
 
     //Setup before any of the tests are started
     @BeforeClass
     public static void initProvider() throws Exception {
-        ProductionManager.initializeProvider("unittest");
-        daoManager = ProductionManager.getInstance();
+        ProductionProvider.initializeProvider("unittest");
+        daoProvider = ProductionProvider.getInstance();
+        daoManager = daoProvider.getDaoManager();
     }
 
     //Gets executed after all tests have been run
     @AfterClass
     public static void closeProvider() throws Exception {
         daoManager.close();
+        daoProvider.close();
     }
 
 
@@ -45,28 +48,33 @@ public class ProductionFunctionDAOTest {
         //test if a function can be succesfully added to the database
         adr1 = new Address("streettest n1", "59", "town 1", "9999", "country 1");
 
-        try (CustomerDAO customerDAO = daoManager.getCustomerDAO();) {
+        try {
+            CustomerDAO customerDAO = daoManager.getCustomerDAO();
             cust1 = customerDAO.create(new Customer(adr1, "911", "customername 1", "btw123"));
         } catch (Exception e) {
             fail("Failed trying to create a new customer");
         }
-        try (UserDAO userDAO = daoManager.getUserDAO();) {
+        try {
+            UserDAO userDAO = daoManager.getUserDAO();
             usr1 = userDAO.create(new User("Firstname 1", "Lastname 1", "Email@address1.com", "hashedPassword1"));
         } catch (Exception e) {
             fail("Failed trying to create a new user");
         }
-        try (RoleDAO roleDAO = daoManager.getRoleDAO();) {
+        try {
+            RoleDAO roleDAO = daoManager.getRoleDAO();
             r1 = roleDAO.create(new Role("testRole1"));
         } catch (Exception e) {
             fail("Failed trying to create a new role");
         }
-        try (FunctionDAO functionDAO = daoManager.getFunctionDAO();) {
+        try {
+            FunctionDAO functionDAO = daoManager.getFunctionDAO();
             f1 = functionDAO.create(new Function(cust1, r1, usr1, LocalDateTime.of(2016, 7, 15, 0, 0), LocalDateTime.of(2017, 8, 3, 0, 0)));
         } catch (Exception e) {
             fail("Failed trying to create a new function");
         }
         //If a function was succesfully added, test if it can be retrieved succesfully and if all fields were correctly set
-        try (FunctionDAO functionDAO = daoManager.getFunctionDAO();) {
+        try {
+            FunctionDAO functionDAO = daoManager.getFunctionDAO();
             if (f1 != null) {
                 Function f2 = functionDAO.get(f1.getUuid());
                 assertEquals("customer field not created correctly", f1.getCompany(), f2.getCompany());
@@ -80,7 +88,8 @@ public class ProductionFunctionDAOTest {
             fail("Failed trying to get an existing function from the database");
         }
         //If the function is confirmed to be present in the database, try to remove it
-        try (FunctionDAO functionDAO = daoManager.getFunctionDAO();) {
+        try {
+            FunctionDAO functionDAO = daoManager.getFunctionDAO();
             if (f1 != null && present) {
                 functionDAO.remove(f1.getUuid());
                 removed = true;
@@ -89,7 +98,8 @@ public class ProductionFunctionDAOTest {
             fail("Failed trying to remove an function from the database");
         }
         //Check if the function is effectively removed (if create, get and remove tests passed)
-        try (FunctionDAO functionDAO = daoManager.getFunctionDAO();) {
+        try {
+            FunctionDAO functionDAO = daoManager.getFunctionDAO();
             if (f1 != null && present && removed) {
                 Function f2 = functionDAO.get(f1.getUuid());
                 //adding this because I'm not sure if the get method returns a null object or an error for a non existing uuid
@@ -101,62 +111,62 @@ public class ProductionFunctionDAOTest {
             //Nothing because the test passed in this case
         }
         //make sure everything is removed from the database again
-        try (CustomerDAO customerDAO = daoManager.getCustomerDAO();
-             AddressDAO addressDAO = daoManager.getAddressDao();
-             UserDAO userDAO = daoManager.getUserDAO();
-             RoleDAO roleDAO = daoManager.getRoleDAO();) {
-            if (r1 != null) {
-                roleDAO.remove(r1.getUuid());
-            }
-            if (usr1 != null) {
-                userDAO.remove(usr1.getUuid());
-            }
-            if (cust1 != null) {
-                customerDAO.remove(cust1.getUuid());
-            }
+        CustomerDAO customerDAO = daoManager.getCustomerDAO();
+        AddressDAO addressDAO = daoManager.getAddressDao();
+        UserDAO userDAO = daoManager.getUserDAO();
+        RoleDAO roleDAO = daoManager.getRoleDAO();
+        if (r1 != null) {
+            roleDAO.remove(r1.getUuid());
         }
+        if (usr1 != null) {
+            userDAO.remove(usr1.getUuid());
+        }
+        if (cust1 != null) {
+            customerDAO.remove(cust1.getUuid());
+        }
+
     }
 
     @Test
     public void update() throws Exception {
-        try (FunctionDAO functionDAO = daoManager.getFunctionDAO();
-             CustomerDAO customerDAO = daoManager.getCustomerDAO();
-             AddressDAO addressDAO = daoManager.getAddressDao();
-             UserDAO userDAO = daoManager.getUserDAO();
-             RoleDAO roleDAO = daoManager.getRoleDAO();) {
-            Address adr1 = addressDAO.create(new Address("streettest n1", "59", "town 1", "9999", "country 1"));
-            Customer cust1 = customerDAO.create(new Customer(adr1, "911", "customername 1", "btw123"));
-            User usr1 = userDAO.create(new User("Firstname 1", "Lastname 1", "Email@address1.com", "hashedPassword1"));
-            Role r1 = roleDAO.create(new Role("testRole1"));
+        FunctionDAO functionDAO = daoManager.getFunctionDAO();
+        CustomerDAO customerDAO = daoManager.getCustomerDAO();
+        AddressDAO addressDAO = daoManager.getAddressDao();
+        UserDAO userDAO = daoManager.getUserDAO();
+        RoleDAO roleDAO = daoManager.getRoleDAO();
+        Address adr1 = addressDAO.create(new Address("streettest n1", "59", "town 1", "9999", "country 1"));
+        Customer cust1 = customerDAO.create(new Customer(adr1, "911", "customername 1", "btw123"));
+        User usr1 = userDAO.create(new User("Firstname 1", "Lastname 1", "Email@address1.com", "hashedPassword1"));
+        Role r1 = roleDAO.create(new Role("testRole1"));
 
-            Address adr2 = addressDAO.create(new Address("streettest n2", "60", "town 2", "99999", "country 2"));
-            Customer cust2 = customerDAO.create(new Customer(adr2, "912", "customername 2", "btw124"));
-            User usr2 = userDAO.create(new User("Firstname 2", "Lastname 2", "Email@address2.com", "hashedPassword2"));
-            Role r2 = roleDAO.create(new Role("testRole2"));
-            LocalDateTime t1 = LocalDateTime.of(2017, 7, 15, 0, 0);
-            LocalDateTime t2 = LocalDateTime.of(2018, 8, 3, 0, 0);
+        Address adr2 = addressDAO.create(new Address("streettest n2", "60", "town 2", "99999", "country 2"));
+        Customer cust2 = customerDAO.create(new Customer(adr2, "912", "customername 2", "btw124"));
+        User usr2 = userDAO.create(new User("Firstname 2", "Lastname 2", "Email@address2.com", "hashedPassword2"));
+        Role r2 = roleDAO.create(new Role("testRole2"));
+        LocalDateTime t1 = LocalDateTime.of(2017, 7, 15, 0, 0);
+        LocalDateTime t2 = LocalDateTime.of(2018, 8, 3, 0, 0);
 
-            Function f1 = functionDAO.create(new Function(cust1, r1, usr1, LocalDateTime.of(2016, 7, 15, 0, 0), LocalDateTime.of(2017, 8, 3, 0, 0)));
-            f1.setCompany(cust2);
-            f1.setRole(r2);
-            f1.setUser(usr2);
-            f1.setStartDate(t1);
-            f1.setEndDate(t2);
-            functionDAO.update(f1);
-            Function f3 = functionDAO.get(f1.getUuid());
-            assertEquals("customer field not updated correctly", cust2, f3.getCompany());
-            assertEquals("role field not updated correctly", r2, f3.getRole());
-            assertEquals("user field not updated correctly", usr2, f3.getUser());
-            assertEquals("startDate field not updated correctly", t1, f3.getStartDate());
-            assertEquals("endDate field not updated correctly", t2, f3.getEndDate());
+        Function f1 = functionDAO.create(new Function(cust1, r1, usr1, LocalDateTime.of(2016, 7, 15, 0, 0), LocalDateTime.of(2017, 8, 3, 0, 0)));
+        f1.setCompany(cust2);
+        f1.setRole(r2);
+        f1.setUser(usr2);
+        f1.setStartDate(t1);
+        f1.setEndDate(t2);
+        functionDAO.update(f1);
+        Function f3 = functionDAO.get(f1.getUuid());
+        assertEquals("customer field not updated correctly", cust2, f3.getCompany());
+        assertEquals("role field not updated correctly", r2, f3.getRole());
+        assertEquals("user field not updated correctly", usr2, f3.getUser());
+        assertEquals("startDate field not updated correctly", t1, f3.getStartDate());
+        assertEquals("endDate field not updated correctly", t2, f3.getEndDate());
 
-            functionDAO.remove(f1.getUuid());
-            roleDAO.remove(r1.getUuid());
-            roleDAO.remove(r2.getUuid());
-            userDAO.remove(usr1.getUuid());
-            userDAO.remove(usr2.getUuid());
-            customerDAO.remove(cust1.getUuid());
-            customerDAO.remove(cust2.getUuid());
-        }
+        functionDAO.remove(f1.getUuid());
+        roleDAO.remove(r1.getUuid());
+        roleDAO.remove(r2.getUuid());
+        userDAO.remove(usr1.getUuid());
+        userDAO.remove(usr2.getUuid());
+        customerDAO.remove(cust1.getUuid());
+        customerDAO.remove(cust2.getUuid());
+
     }
 }
