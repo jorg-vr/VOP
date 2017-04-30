553252285997b8374486c2d88cc9a249d1de60b9
Sam Persoon <sam.persoon@ugent.be>
2017-04-30 11:38:06 +0200

    Remove sessionFactory from productionmanager

--- app/backend/src/main/java/dao/database/ProductionManager.java
+++ app/backend/src/main/java/dao/database/ProductionManager.java
@@ -3,153 +3,110 @@ package dao.database;
 import dao.interfaces.*;
 import model.identity.Company;
 import model.insurance.Surety;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 
 public class ProductionManager implements DAOManager {
 
-    private static ProductionManager provider = null;
-    private StandardServiceRegistry registry;
-    private SessionFactory sessionFactory;
+    private Session session;
 
 
-    private ProductionManager(String configLocation) {
-
-        registry = new StandardServiceRegistryBuilder()
-                .configure(configLocation)
-                .build();
-        // Create MetadataSources
-        MetadataSources sources = new MetadataSources(registry);
-
-        // Create Metadata
-        Metadata metadata = sources.getMetadataBuilder().build();
-
-        // Create SessionFactory
-        sessionFactory = metadata.getSessionFactoryBuilder().build();
-
-    }
-
-    /**
-     * SHOULD BE CALLED BEFORE getInstance()
-     * initializes the provider with the right hibernate configuration
-     *
-     * @param environment should it run on production or development
-     */
-    public synchronized static void initializeProvider(String environment) {
-        if (environment.equals("production")) {
-            provider = new ProductionManager("hibernate/deployment.cfg.xml");
-        } else if (environment.equals("localtest")) {
-            provider = new ProductionManager("hibernate/localtest.cfg.xml");
-        } else if (environment.equals("test")) {
-            provider = new ProductionManager("hibernate/test.cfg.xml");
-        } else if (environment.equals("unittest")) {
-            provider = new ProductionManager("hibernate/test-in-memory.cfg.xml");
-        }
-
-    }
-
-    /**
-     * @return the DAOManager
-     */
-    public synchronized static DAOManager getInstance() {
-        return provider;
+    ProductionManager(Session session) {
+        this.session = session;
     }
 
     @Override
     public UserDAO getUserDAO() {
-        return new ProductionUserDAO(sessionFactory.openSession());
+        return new ProductionUserDAO(session);
     }
 
     @Override
     public synchronized CustomerDAO getCustomerDAO() {
-        return new ProductionCustomerDAO(sessionFactory.openSession());
+        return new ProductionCustomerDAO(session);
     }
 
     @Override
     public synchronized CompanyDAO<Company> getCompanyDAO() {
-        return new ProductionCompanyDAO(sessionFactory.openSession());
+        return new ProductionCompanyDAO(session);
     }
 
     @Override
     public synchronized FleetDAO getFleetDAO() {
-        return new ProductionFleetDAO(sessionFactory.openSession());
+        return new ProductionFleetDAO(session);
     }
 
     @Override
     public synchronized FunctionDAO getFunctionDAO() {
-        return new ProductionFunctionDAO(sessionFactory.openSession());
+        return new ProductionFunctionDAO(session);
     }
 
     @Override
     public synchronized VehicleDAO getVehicleDAO() {
-        return new ProductionVehicleDAO(sessionFactory.openSession());
+        return new ProductionVehicleDAO(session);
     }
 
     @Override
     public synchronized VehicleTypeDAO getVehicleTypeDAO() {
-        return new ProductionVehicleTypeDAO(sessionFactory.openSession());
+        return new ProductionVehicleTypeDAO(session);
     }
 
     @Override
     public AddressDAO getAddressDao() {
-        return new ProductionAddressDAO(sessionFactory.openSession());
+        return new ProductionAddressDAO(session);
     }
 
     @Override
     public RoleDAO getRoleDAO() {
-        return new ProductionRoleDAO(sessionFactory.openSession());
+        return new ProductionRoleDAO(session);
     }
 
     @Override
     public ContractDAO getContractDao() {
-        return new ProductionContractDAO(sessionFactory.openSession());
+        return new ProductionContractDAO(session);
     }
 
     @Override
     public SuretyDAO<Surety> getSuretyDao() {
-        return new ProductionSuretyDAO(sessionFactory.openSession());
+        return new ProductionSuretyDAO(session);
     }
 
     @Override
     public NonFlatSuretyDAO getFlatSuretyDao() {
-        return new ProductionNonFlatSuretyDAO(sessionFactory.openSession());
+        return new ProductionNonFlatSuretyDAO(session);
     }
 
     @Override
     public InvoiceDAO getInvoiceDao() {
-        return new ProductionInvoiceDAO(sessionFactory.openSession());
+        return new ProductionInvoiceDAO(session.openSession());
     }
 
     @Override
     public FlatSuretyDAO getNonFlatSuretyDao() {
-        return new ProductionFlatSuretyDAO(sessionFactory.openSession());
+        return new ProductionFlatSuretyDAO(session.openSession());
     }
 
     @Override
     public SpecialConditionDAO getSpecialConditionDao() {
-        return new ProductionSpecialConditionDAO(sessionFactory.openSession());
+        return new ProductionSpecialConditionDAO(session.openSession());
     }
 
 
     @Override
     public VehicleInsuranceDAO getVehicleInsuranceDao() {
-        return new ProductionVehicleInsuranceDAO(sessionFactory.openSession());
+        return new ProductionVehicleInsuranceDAO(session.openSession());
     }
 
     @Override
     public InsuranceCompanyDAO getInsuranceCompanyDao() {
-        return new ProductionInsuranceCompanyDAO(sessionFactory.openSession());
+        return new ProductionInsuranceCompanyDAO(session.openSession());
     }
 
 
     @Override
     public void close() {
-        sessionFactory.close();
-        StandardServiceRegistryBuilder.destroy(this.registry);
-        provider = null;
+        session.close();
     }
 }
