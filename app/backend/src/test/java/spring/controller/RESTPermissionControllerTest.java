c083c19418468dae4e33404e4769f638ba4e0fe4
tdpontie <thomas.depontieu@ugent.be>
2017-05-05 01:02:36 +0200

    remove RESTUserController test

--- app/backend/src/test/java/spring/controller/RESTPermissionControllerTest.java
+++ app/backend/src/test/java/spring/controller/RESTPermissionControllerTest.java
@@ -112,7 +112,7 @@ public class RESTPermissionControllerTest {
         assertTrue("Permissions were not correctly updated",role.hasAccess(Resource.BILLING, Action.READ_MINE));
         assertTrue("Permissions were not correctly updated",role.hasAccess(Resource.BILLING, Action.CREATE_ALL));
         assertTrue("Permissions were not correctly updated",role.hasAccess(Resource.FLEET, Action.READ_ALL));
-        
+
         //Clean up database for other tests
         roleDAO.remove(role.getUuid());
 
