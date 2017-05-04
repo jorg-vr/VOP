dbcf8cd30762c087b4d4921eb2e6c003b1b229fb
Sam Persoon <sam.persoon@ugent.be>
2017-05-04 13:51:54 +0200

    Finished exceptions

--- app/backend/src/main/java/dao/database/HibernateUtil.java
+++ app/backend/src/main/java/dao/database/HibernateUtil.java
@@ -1,7 +1,8 @@
 package dao.database;
 
 import dao.database.util.unique.ConstraintValidatorFactoryImpl;
-import dao.exceptions.DataAccessException;
+import dao.exceptions.*;
+import dao.exceptions.ConstraintViolationException;
 import dao.interfaces.DAOManager;
 import dao.interfaces.DAOProvider;
 import dao.interfaces.UserDAO;
@@ -20,6 +21,10 @@ import java.util.Set;
  */
 public class HibernateUtil {
 
+    private static ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+    private static ValidatorContext validatorContext = validatorFactory.usingContext();
+
+
     /**
      * Using Hibernate make the given object persistent in the database
      *
@@ -27,13 +32,15 @@ public class HibernateUtil {
      * @param objectToSave The object to save
      * @throws DataAccessException Thrown when constraints are violated or session
      */
-    public synchronized static void create(Session session, Object objectToSave) throws DataAccessException {
+    public synchronized static void create(Session session, Object objectToSave) throws DataAccessException, ConstraintViolationException {
         Transaction transaction = null;
         try {
             transaction = session.beginTransaction();
             validate(session,objectToSave);
             session.save(objectToSave);
             transaction.commit();
+        } catch (ConstraintViolationException e){
+            throw e;
         } catch (Exception e) {
             e.printStackTrace();
             if (transaction != null) {
@@ -57,11 +64,11 @@ public class HibernateUtil {
             session.delete(objectToRemove);
             tx.commit();
         } catch (Exception e) {
-
             e.printStackTrace();
             if (tx != null) {
                 tx.rollback();
             }
+            throw new DataAccessException();
         }
     }
 
@@ -73,13 +80,15 @@ public class HibernateUtil {
      * @param objectToUpdate The object to update
      * @throws DataAccessException Thrown when constraints are violated or session
      */
-    public synchronized static void update(Session session, Object objectToUpdate) throws DataAccessException {
+    public synchronized static void update(Session session, Object objectToUpdate) throws DataAccessException, ConstraintViolationException {
         Transaction tx = null;
         try {
             tx = session.beginTransaction();
             validate(session,objectToUpdate);
             session.merge(objectToUpdate);
             tx.commit();
+        } catch(ConstraintViolationException e){
+            throw e;
         } catch (Exception e) {
             if (tx != null) {
                 tx.rollback();
@@ -89,43 +98,19 @@ public class HibernateUtil {
         }
     }
 
-    private synchronized static Map<String, String> validate(Session session, Object object) {
+    private synchronized static void validate(Session session, Object object) throws ConstraintViolationException {
         Map<String, String> map = new HashMap<>();
-        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
-        ValidatorContext validatorContext = validatorFactory.usingContext();
         validatorContext.constraintValidatorFactory(
                 new ConstraintValidatorFactoryImpl
                         (session));
         Validator validator = validatorContext.getValidator();
-
         Set<ConstraintViolation<Object>> violations = validator.validate(object);
 
         for (ConstraintViolation<Object> violation : violations) {
             map.put(violation.getPropertyPath().toString(), violation.getMessage());
-            System.out.println(violation.getPropertyPath() + " " + violation.getMessage());
         }
-//        throw new RuntimeException();
-        return map;
-    }
-
-    public static void main(String[] args) {
-        ProductionProvider.initializeProvider("unittest");
-        try (DAOProvider provider = ProductionProvider.getInstance();
-             DAOManager daoManager = provider.getDaoManager()) {
-            UserDAO userDAO = daoManager.getUserDAO();
-            User user = new User();
-            user.setFirstName("test");
-            user.setLastName("test");
-            //user.setEmail("test,test");
-            userDAO.create(user);
-            //userDAO.remove(user.getUuid());
-            User user1 = new User();
-            user1.setFirstName("test");
-            user1.setLastName("test");
-            user1.setEmail("test,test");
-            userDAO.create(user1);
-        } catch (Exception e) {
-            e.printStackTrace();
+        if(map.size()>0){
+            throw new ConstraintViolationException(map);
         }
     }
 }
