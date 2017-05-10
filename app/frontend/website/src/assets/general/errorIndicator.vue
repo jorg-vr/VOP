1554ed0f972424d63ced105581815ccd95154cb7
Jarre Knockaert <jarknock@ugent.be>
2017-05-10 08:17:20 +0200

    add error indicator

---
+++ app/frontend/website/src/assets/general/errorIndicator.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="row">
+        <div v-if="visible && error" class="alert alert-danger alert-dismissible col-lg-8 col-md-9 col-sm-11" role="danger">
+            <button type="button" class="close" @click="visible=false" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <strong>{{error.error}}:</strong> The page failed to load.
+        </div>
+    </div>
+
+</template>
+<script>
+    import {mapGetters} from 'vuex'
+
+    export default {
+        data() {
+            return {
+                visible: true
+            }
+        },
+        computed: {
+            ...mapGetters([
+                'error'
+            ])
+        }
+    }
+</script>
+<style>
+
+</style>
--- app/frontend/website/src/store/mutations.js
+++ app/frontend/website/src/store/mutations.js
@@ -10,8 +10,22 @@ export default {
         state.loading = loading
     },
 
+    /**
+     * Add a route which has been visited to the stack.
+     * @param state
+     * @param route
+     */
     pushVisitedRoute(state, {route}){
         state.visitedRoutes.push(route)
+    },
+
+    /**
+     * Set the latest error returned by the API
+     * @param state
+     * @param response
+     */
+    setError(state, response){
+        state.error = response.body
     }
 
 }
