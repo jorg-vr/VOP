0c1e1911a0fc47abacd7579ca801589fa28e10ea
bwdvolde <billie.devolder@ugent.be>
2017-05-11 18:33:50 +0200

    Add examples for importing and getting files

--- app/backend/src/main/java/spring/files/FileUploadController.java
+++ app/backend/src/main/java/spring/files/FileUploadController.java
@@ -1,17 +1,16 @@
 package spring.files;
 
+import csv.CSVtoVehicleParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import spring.files.storage.StorageFileNotFoundException;
 import spring.files.storage.StorageService;
+import spring.model.RESTVehicle;
 
-import java.io.IOException;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
@@ -25,35 +24,27 @@ public class FileUploadController {
         this.storageService = storageService;
     }
 
-    @GetMapping("/files")
-    public Collection<String> listUploadedFiles() throws IOException {
-        return storageService
-                .loadAll()
-                .map(path ->
-                        MvcUriComponentsBuilder
-                                .fromMethodName(FileUploadController.class, "serveFile", path.getFileName().toString())
-                                .build().toString())
-                .collect(Collectors.toList());
-
-    }
-
     @GetMapping("/files/{filename:.+}")
     @ResponseBody
     public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
         Resource file = storageService.loadAsResource(filename);
         return ResponseEntity
                 .ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+file.getFilename()+"\"")
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
                 .body(file);
     }
 
-    @PostMapping("/upload")
-    public void handleFileUpload(@RequestParam("file") MultipartFile file,
-                                   RedirectAttributes redirectAttributes) {
-
-        storageService.store(file);
-        redirectAttributes.addFlashAttribute("message",
-                "You successfully uploaded " + file.getOriginalFilename() + "!");
+    @PostMapping("/${path.vehicles}/${path.import}")
+    public Collection<RESTVehicle> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        try {
+            return CSVtoVehicleParser.parse(file.getInputStream())
+                    .stream()
+                    .map(RESTVehicle::new)
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @ExceptionHandler(StorageFileNotFoundException.class)
