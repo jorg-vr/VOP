a4236eab47d5b55e2b94888205be917a7a015c6d
Tomas Van Roose <tomas.vanroose@ugent.be>
2017-05-12 10:59:57 +0200

    Use createCompany

--- app/backend/src/main/java/dao/database/util/RealDataDatabaseFiller.java
+++ app/backend/src/main/java/dao/database/util/RealDataDatabaseFiller.java
@@ -29,12 +29,9 @@ public class RealDataDatabaseFiller {
 
     private void initUsers(DAOProvider provider) {
         try(DAOManager manager = provider.getDaoManager()){
-            Company company = new Company();
-            company.setCompanyType(CompanyType.CUSTOMER);
-            company.setPhoneNumber("093725663");
-            company.setName("Solvas");
-            company.setAddress(createAddress("Kerkstraat","1","Zomergem","9930","België"));
 
+            Address address = createAddress("Kerkstraat","1","Zomergem","9930","België");
+            Company company = createCompany(CompanyType.CUSTOMER,"093725663","Solvas", address );
 
             Function function = new Function();
             function.setCompany(company);
