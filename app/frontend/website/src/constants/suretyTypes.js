d097237e081905a39d1963d596d45ab9f986020b
Jorg Van Renterghem <jorg.vanrenterghem@ugent.be>
2017-05-13 01:28:57 +0200

    Create and eddit taxes

--- app/frontend/website/src/constants/suretyTypes.js
+++ app/frontend/website/src/constants/suretyTypes.js
@@ -1,37 +1,37 @@
 import Vue from 'vue'
 
-export default {
-    CIVIL_LIABILITY: {
+export default [
+    {
         name: 'CIVIL_LIABILITY',
         translation: function(){
             return Vue.t('suretyTypes.CIVIL_LIABILITY')
         }
     },
-    OMNIUM_FULL: {
+    {
         name: 'OMNIUM_FULL',
         translation: function(){
             return Vue.t('suretyTypes.OMNIUM_FULL')
         }
     },
-    OMNIUM_PARTIAL: {
+    {
         name: 'OMNIUM_PARTIAL',
         translation: function(){
             return Vue.t('suretyTypes.OMNIUM_PARTIAL')
         }
     },
-    LEGAL_AID: {
+    {
         name: 'LEGAL_AID',
         translation: function(){
             return Vue.t('suretyTypes.LEGAL_AID')
         }
     },
-    TRAVEL_AID: {
+    {
         name: 'TRAVEL_AID',
         translation: function(){
             return Vue.t('suretyTypes.TRAVEL_AID')
         }
     },
-    SAFETY: {
+    {
         name: 'SAFETY',
         translation: function(){
             return Vue.t('suretyTypes.SAFETY')
@@ -39,4 +39,4 @@ export default {
     }
 
 
-}
+]
--- app/frontend/website/src/pages/vehicleType/vehicleTypeForm.vue
+++ app/frontend/website/src/pages/vehicleType/vehicleTypeForm.vue
@@ -1,16 +1,37 @@
 <template>
     <div>
         <text-input-form-group :object="vehicleType" name="name" :text="$t('vehicleType.vehicleType')" :rules="'required'"></text-input-form-group>
+        <div  v-for="tax in vehicleType.taxes">
+            <text-input-form-group
+
+                    :object="tax"
+                    name="tax"
+                    :text="tax.suretyTypeTr"
+                    :rules="'required'"
+            ></text-input-form-group>
+        </div>
     </div>
 
 </template>
 <script>
     import {mapGetters, mapActions} from 'vuex'
     import textInputFormGroup from '../../assets/form/FormGroups/TextInputFormGroup.vue'
+    import suretyTypes from '../../constants/suretyTypes'
 
     export default {
         props: {
             vehicleType: Object
+        },
+        created(){
+            if(this.vehicleType.taxes==undefined){
+                this.vehicleType.taxes=[];
+                for(let i=0;i<suretyTypes.length;i++){
+                    this.vehicleType.taxes[i]={};
+                    this.vehicleType.taxes[i].suretyType=suretyTypes[i].name;
+                    this.vehicleType.taxes[i].suretyTypeTr=suretyTypes[i].translation();
+                    this.vehicleType.taxes[i].tax=0;
+                }
+            }
         },
         components: {
             textInputFormGroup
