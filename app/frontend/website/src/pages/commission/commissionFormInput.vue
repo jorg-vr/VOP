20c071b1f93b1c6d5db1326b0c7274444224d2d2
Jarre Knockaert <jarre.knockaert@ugent.be>
2017-05-14 18:13:10 +0200

    local changes

---
+++ app/frontend/website/src/pages/commission/commissionFormInput.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div  v-for="commission in commissions">
+            <text-input-form-group
+                    :object="commission"
+                    name="commission"
+                    :text="$t('suretyTypes.'+commission.suretyType)"
+                    :rules="'required'"
+            ></text-input-form-group>
+        </div>
+    </div>
+
+</template>
+<script>
+    import {mapGetters, mapActions} from 'vuex'
+    import textInputFormGroup from '../../assets/form/FormGroups/TextInputFormGroup.vue'
+    import suretyTypes from '../../constants/suretyTypes'
+
+    export default {
+        data(){
+            return {
+                commissions: {}
+            }
+        },
+        created(){
+            this.$store.dispatch('fetchCommissions').then(commissions => {
+                this.commissions = commissions
+            })
+        },
+        components: {
+            textInputFormGroup
+        }
+    }
+</script>
