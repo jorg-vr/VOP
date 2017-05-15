4c3236e31ddafc1b4afc6ce52d923dca72bdfd5c
Jarre Knockaert <jarknock@ugent.be>
2017-05-15 07:58:37 +0200

    undo deletion of commission edit

---
+++ app/frontend/website/src/pages/commission/edit.vue
@@ -0,0 +1,46 @@
+<template>
+    <abstract-form :actions="actions" :object="comissions" :back="back" :resource="resource" :ids="ids">
+        <commission-form-input :object="commissions"></commission-form-input>
+    </abstract-form>
+</template>
+<script>
+    import actions from '../../constants/actions'
+    import resources from '../../constants/resources'
+    import commissionFormInput from '../commission/commissionFormInput.vue'
+    import { mapGetters, mapActions, mapMutations } from 'vuex'
+    import abstractForm from '../../assets/form/AbstractForm.vue'
+
+    export default {
+        data(){
+            return {
+                actions: actions.UPDATE,
+                resource: resources.COMMISSION,
+                ids: {'resource':this.loc,'resourceId':this.id}
+            }
+        },
+        created(){
+            if(this.id){
+                this.fetchCommissions({ids: this.ids});
+            }
+        },
+        components: {
+            abstractForm,
+            commissionFormInput
+        },
+        props: {
+            back: Object,
+            id: String,
+            loc:String
+        },
+        computed: {
+            ...mapGetters([
+                'commissions'
+            ])
+        },
+        methods: {
+            ...mapActions([
+                'fetchCommissions'
+            ])
+        }
+    }
+</script>
