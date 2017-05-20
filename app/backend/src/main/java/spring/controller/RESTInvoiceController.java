e3792aa04aba29e2b168675705fd0ab32ae49b21
bwdvolde <billie.devolder@ugent.be>
2017-05-20 11:54:11 +0200

    Rename vehicleInvoice property to vehicle_invoice

--- app/backend/src/main/java/spring/controller/RESTInvoiceController.java
+++ app/backend/src/main/java/spring/controller/RESTInvoiceController.java
@@ -73,7 +73,7 @@ public class RESTInvoiceController extends RESTAbstractController<RESTInvoice, I
         }
     }
 
-    @RequestMapping(value = "/{id}/${path.vehicleInvoices}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}/${path.vehicle_nvoices}", method = RequestMethod.GET)
     public RESTSchema<RESTVehicleInvoice> getAllVehicleInvoices(@PathVariable String companyId,
                                                                 @PathVariable String id,
                                                                 HttpServletRequest request,
